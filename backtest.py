8dcba00dc4185f6ee80287979a1fcbcc638583fc
enarjord <eirik.narjord@gmail.com>
2020-12-28 11:31:22 +0100

    Update backtest.py

--- backtest.py
+++ backtest.py
@@ -1,3 +1,4 @@
+import sys
 import json
 import numpy as np
 import pandas as pd
@@ -7,6 +8,7 @@ from time import time
 from passivbot import init_ccxt, make_get_filepath, ts_to_date, print_, load_settings
 from binance import fetch_trades as binance_fetch_trades
 from bybit import fetch_trades as bybit_fetch_trades
+from bybit import round_up, round_dn
 from typing import Iterator
 
 
@@ -14,7 +16,9 @@ def backtest(adf: pd.DataFrame,
              settings: dict,
              margin_cost_limit: float = 0.0,
              maker_fee: float = 0.00018,
-             taker_fee: float = 0.00036) -> ([dict], [dict], pd.DataFrame):
+             taker_fee: float = 0.00036,
+             price_step: float = 0.01,
+             inverse: bool = False) -> ([dict], [dict], pd.DataFrame):
     ema_spans = sorted(settings['ema_spans'])
     markup = settings['markup']
     leverage = settings['leverage']
@@ -23,6 +27,14 @@ def backtest(adf: pd.DataFrame,
     enter_shrt = settings['enter_shrt']
     spread_minus = 1 - settings['spread']
     spread_plus = 1 + settings['spread']
+
+    rdn = lambda n: round_dn(n, price_step)
+    rup = lambda n: round_up(n, price_step)
+
+    if inverse:
+        calc_cost = lambda amount, price: amount / price
+    else:
+        calc_cost = lambda amount, price: amount * price
 
     assert enter_long or enter_shrt
 
@@ -61,12 +73,12 @@ def backtest(adf: pd.DataFrame,
     for row in adf.drop([c for c in adf.columns if 'ema' in c], axis=1).itertuples():
         do_print = False
         if pos_amount == 0.0:
-            if row.price < getattr(row, min_name) * spread_minus:
+            if row.price < rdn(getattr(row, min_name) * spread_minus):
                 pos_amount = entry_amount
                 entry_price = row.price
-                liq_price = entry_price * (1 - liq_multiplier)
-                exit_price = entry_price * (1 + markup)
-                cost = entry_amount * entry_price
+                liq_price = rdn(entry_price * (1 - liq_multiplier))
+                exit_price = rup(entry_price * (1 + markup))
+                cost = calc_cost(entry_amount, entry_price)
                 trades.append({'timestamp': row.timestamp, 'side': 'buy', 'type': 'entry',
                                'trade_id': row.Index, 'price': entry_price,
                                'amount': pos_amount,
@@ -74,12 +86,12 @@ def backtest(adf: pd.DataFrame,
                                'fee': cost * taker_fee,
                                'n_double_downs': -1})
                 do_print = True
-            elif row.price > getattr(row, max_name) * spread_plus:
+            elif row.price > rup(getattr(row, max_name) * spread_plus):
                 pos_amount = -entry_amount
                 entry_price = row.price
-                liq_price = entry_price * (1 + liq_multiplier)
-                exit_price = entry_price * (1 - markup)
-                cost = entry_amount * entry_price
+                liq_price = rup(entry_price * (1 + liq_multiplier))
+                exit_price = rdn(entry_price * (1 - markup))
+                cost = calc_cost(entry_amount, entry_price)
                 trades.append({'timestamp': row.timestamp, 'side': 'sel', 'type': 'entry',
                                'trade_id': row.Index, 'price': entry_price,
                                'amount': pos_amount,
@@ -89,8 +101,8 @@ def backtest(adf: pd.DataFrame,
                 do_print = True
         elif pos_amount > 0.0:
             # long position
-            if row.price >= exit_price:
-                cost = pos_amount * exit_price
+            if row.price > exit_price:
+                cost = calc_cost(pos_amount, exit_price)
                 realized_pnl = cost * markup
                 trades.append({'timestamp': row.timestamp, 'side': 'sel', 'type': 'exit',
                                'trade_id': row.Index, 'price': exit_price, 'amount': pos_amount,
@@ -100,8 +112,8 @@ def backtest(adf: pd.DataFrame,
                 do_print = True
                 pos_amount = 0.0
                 n_double_downs = 0
-            elif row.price <= liq_price:
-                cost = pos_amount * liq_price
+            elif row.price < liq_price:
+                cost = calc_cost(pos_amount, liq_price)
                 margin_cost = cost / leverage
                 if margin_cost >= margin_cost_limit / 2:
                     # liquidation
@@ -125,13 +137,13 @@ def backtest(adf: pd.DataFrame,
                                    'n_double_downs': n_double_downs})
                     entry_price = (entry_price + liq_price) / 2
                     pos_amount *= 2
-                    liq_price = entry_price * (1 - liq_multiplier)
-                    exit_price = entry_price * (1 + markup)
+                    liq_price = rdn(entry_price * (1 - liq_multiplier))
+                    exit_price = rup(entry_price * (1 + markup))
                     do_print = True
         else:
             # shrt position
-            if row.price <= exit_price:
-                cost = -pos_amount * exit_price
+            if row.price < exit_price:
+                cost = calc_cost(-pos_amount, exit_price)
                 realized_pnl = cost * markup
                 trades.append({'timestamp': row.timestamp, 'side': 'buy', 'type': 'exit',
                                'trade_id': row.Index, 'price': exit_price, 'amount': pos_amount,
@@ -141,13 +153,11 @@ def backtest(adf: pd.DataFrame,
                 do_print = True
                 pos_amount = 0.0
                 n_double_downs = 0
-            elif row.price >= liq_price:
-                cost = -pos_amount * liq_price
+            elif row.price > liq_price:
+                cost = calc_cost(-pos_amount, liq_price)
                 margin_cost = cost / leverage
-                #print('margin_cost', margin_cost)
                 if margin_cost >= margin_cost_limit / 2:
                     # liquidation
-                    cost = -pos_amount * exit_price
                     realized_pnl = -margin_cost
                     trades.append({'timestamp': row.timestamp, 'side': 'buy', 'type': 'liquidation',
                                    'trade_id': row.Index, 'price': exit_price, 'amount': pos_amount,
@@ -166,19 +176,16 @@ def backtest(adf: pd.DataFrame,
                                    'n_double_downs': n_double_downs})
                     entry_price = (entry_price + liq_price) / 2
                     pos_amount *= 2
-                    liq_price = entry_price * (1 + liq_multiplier)
-                    exit_price = entry_price * (1 - markup)
+                    liq_price = rup(entry_price * (1 + liq_multiplier))
+                    exit_price = rdn(entry_price * (1 - markup))
                     do_print = True
         if do_print:
             realized_pnl_sum += trades[-1]['realized_pnl'] - trades[-1]['fee']
             margin_cost_max = max(margin_cost_max, trades[-1]['margin_cost'])
-            line = f'\r{(row.Index - adf.index[0]) / idxrange:.4f} {realized_pnl_sum:.2f} '
-            line += f'{margin_cost_max:.2f} '
+            rounding = 8 if inverse else 2
+            line = f'\r{(row.Index - adf.index[0]) / idxrange:.4f} {realized_pnl_sum:.{rounding}f} '
+            line += f'{margin_cost_max:.{rounding}f} '
             print(line, end=' ')
-            #if margin_cost_limit and margin_cost_max >= margin_cost_limit:
-            #    print('margin_cost_limit exceeded')
-            #    break
-
 
     return trades, adf
 
@@ -200,7 +207,7 @@ def get_new_candidate(ranges: dict, best: dict, m=0.2):
     return new_candidate
 
 
-def jackrabbit(agg_trades: pd.DataFrame):
+def jackrabbit(exchange: str, agg_trades: pd.DataFrame):
 
 
     settings = {'ema_spans': [10000, 38036],
@@ -220,13 +227,30 @@ def jackrabbit(agg_trades: pd.DataFrame):
             'markup': 0.0015,
             'spread': 0.0}
 
+    best = {'ema_spans': (39256.0, 90333.0), 'leverage': 79.0,
+            'markup': 0.002025, 'spread': 0.000239}
 
-    ranges = {'ema_spans': (1000, 250000, 0),
+
+    ranges = {'ema_spans': (5000, 250000, 0),
               'leverage':  (40, 125, 0),
               'markup': (0.001, 0.006, 6),
               'spread': (-0.002, 0.002, 6)}
 
-    margin_cost_limit = 160
+
+    if exchange == 'bybit':
+        price_step = 0.5
+        inverse = True
+        margin_cost_limit = 0.0045
+        maker_fee = -0.00025
+        taker_fee = 0.00075
+    elif exchange == 'binance':
+        price_step = 0.01
+        inverse = False
+        margin_cost_limit = 160
+        maker_fee = 0.00018
+        taker_fee = 0.00036
+    else:
+        raise Exception(f'exchage {exchange} not found')
 
     results = {}
     best_gain = 0
@@ -237,16 +261,14 @@ def jackrabbit(agg_trades: pd.DataFrame):
     ms = np.array([1 / (i * 0.1 + 1) for i in range(ks)])
     ms = (ms - ms.min()) / (ms.max() - ms.min())
 
-    #maker_fee = -0.00025
-    #taker_fee = 0.00075
-    maker_fee = 0.00018
-    taker_fee = 0.00036
+    results_filename = make_get_filepath(
+        f'jackrabbit_results/{exchange}/{ts_to_date(time())[:19]}'
+    )
 
-    results_filename = make_get_filepath(f'jackrabbit_results/{ts_to_date(time())[:19]}.txt')
-    results_filename_csv = make_get_filepath(f'jackrabbit_results/{ts_to_date(time())[:19]}.csv')
-
+    min_n_trades = len(agg_trades) / 10000
+    print('min_n_trades', min_n_trades)
     conditions = [
-        lambda r: r['n_trades'] > 100
+        lambda r: r['n_trades'] > len(agg_trades) / 10000
     ]
 
     while k < ks - 1:
@@ -266,7 +288,9 @@ def jackrabbit(agg_trades: pd.DataFrame):
                                     for k_ in settings},
                                    margin_cost_limit=margin_cost_limit,
                                    maker_fee=maker_fee,
-                                   taker_fee=taker_fee)
+                                   taker_fee=taker_fee,
+                                   inverse=inverse,
+                                   price_step=price_step)
             if not trades:
                 print('\nno trades')
                 candidate = get_new_candidate(best)
@@ -286,7 +310,7 @@ def jackrabbit(agg_trades: pd.DataFrame):
                 result['margin_cost_max']
             results[key] = result
             print(f'\n{result}')
-            with open(results_filename, 'a') as f:
+            with open(results_filename + '.txt', 'a') as f:
                 f.write(str(key) + ' ' + str(results[key]) + '\n')
             if result['gain'] > best_gain:
                 if all([condition(result) for condition in conditions]):
@@ -294,7 +318,7 @@ def jackrabbit(agg_trades: pd.DataFrame):
                     best_gain = result['gain']
                     print('\n\nnew best', best, '\n', result, '\n\n')
             candidate = get_new_candidate(ranges, best, m=ms[k])
-            pd.DataFrame(results).to_csv(results_filename_csv)
+            pd.DataFrame(results).to_csv(results_filename + '.csv')
         except KeyboardInterrupt:
             return results
     return results
@@ -317,8 +341,8 @@ def iter_chunks(exchange: str, symbol: str) -> Iterator[pd.DataFrame]:
         yield None
 
 
-async def load_trades(exchange: str, symbol: str, n_days: float) -> pd.DataFrame:
-    cc = init_ccxt(exchange, 'example_user')
+async def load_trades(exchange: str, user: str, symbol: str, n_days: float) -> pd.DataFrame:
+    cc = init_ccxt(exchange, user)
     if exchange == 'binance':
         fetch_trades_func = binance_fetch_trades
     elif exchange == 'bybit':
@@ -405,17 +429,24 @@ async def load_trades(exchange: str, symbol: str, n_days: float) -> pd.DataFrame
 
 
 async def main():
-    n_days = 21
-    exchange = 'binance'
-    symbol = 'BTCUSDT'
-    filename = f'btcusdt_agg_trades_{n_days}_days_{ts_to_date(time())[:10]}.csv'
+    exchange = sys.argv[1]
+    user = sys.argv[2]
+    n_days = int(sys.argv[3])
+    if exchange == 'bybit':
+        symbol = 'BTCUSD'
+    elif exchange == 'binance':
+        symbol = 'BTCUSDT'
+    else:
+        raise Exception(f'exchange {exchange} not found')
+    filename = f'btcusdt_agg_trades_{exchange}_{n_days}_days_{ts_to_date(time())[:10]}.csv'
     if os.path.isfile(filename):
         print('loading trades...')
         agg_trades = pd.read_csv(filename).set_index('trade_id')
     else:
-        agg_trades = await load_trades(exchange, symbol, n_days)
+        print('fetching trades')
+        agg_trades = await load_trades(exchange, user, symbol, n_days)
         agg_trades.to_csv(filename)
-    jackrabbit(agg_trades)
+    jackrabbit(exchange, agg_trades)
 
 
 if __name__ == '__main__':
