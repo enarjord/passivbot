37c955e54328fc2fb5f85ac8884a00f534e7f300
enarjord <eirik.narjord@gmail.com>
2021-01-29 22:11:55 +0100

    Add files via upload

---
+++ start_bot.py
@@ -0,0 +1,23 @@
+from subprocess import Popen
+from time import sleep
+import sys
+
+exchange = sys.argv[1]
+user = sys.argv[2]
+
+max_n_restarts = 30
+
+restart_k = 0
+
+
+while True:
+    print(f"\nStarting {exchange} {user}")
+    p = Popen(f"python3 {exchange}.py {user}", shell=True)
+    p.wait()
+    restart_k += 1
+    if restart_k > max_n_restarts:
+        print('max n restarts reached, aborting')
+        break
+    for k in range(30 - 1, -1, -1):
+        print(f"\rbot stopped, attempting restart in {k} seconds", end='   ')
+        sleep(1)
