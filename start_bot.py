b4431fd241e7582b5352bc24bb44d88b97e2e8ae
enarjord <eirik.narjord@gmail.com>
2021-02-01 11:00:49 +0100

    bug fix
    wrapped autorestarter in a try block, breaking if keyboardinterrupt

--- start_bot.py
+++ start_bot.py
@@ -11,13 +11,16 @@ restart_k = 0
 
 
 while True:
-    print(f"\nStarting {exchange} {user}")
-    p = Popen(f"python3 {exchange}.py {user}", shell=True)
-    p.wait()
-    restart_k += 1
-    if restart_k > max_n_restarts:
-        print('max n restarts reached, aborting')
+    try:
+        print(f"\nStarting {exchange} {user}")
+        p = Popen(f"python3 {exchange}.py {user}", shell=True)
+        p.wait()
+        restart_k += 1
+        if restart_k > max_n_restarts:
+            print('max n restarts reached, aborting')
+            break
+        for k in range(30 - 1, -1, -1):
+            print(f"\rbot stopped, attempting restart in {k} seconds", end='   ')
+            sleep(1)
+    except KeyboardInterrupt:
         break
-    for k in range(30 - 1, -1, -1):
-        print(f"\rbot stopped, attempting restart in {k} seconds", end='   ')
-        sleep(1)
