50c4e8b194392c02753b72f3eafbbbd0f6160232
enarjord <eirik.narjord@gmail.com>
2021-04-13 15:47:28 +0200

    Update backtest.py

--- backtest.py
+++ backtest.py
@@ -730,5 +730,4 @@ async def main(args: list):
 
 
 if __name__ == '__main__':
-    print("backtester doesn't work in v3.3.3 yet")
-    # asyncio.run(main(sys.argv))
+    asyncio.run(main(sys.argv))
