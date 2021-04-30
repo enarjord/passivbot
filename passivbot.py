5958622b4418c050adfa0f1c44f0f0b308fcee94
enarjord <eirik.narjord@gmail.com>
2021-04-30 21:21:13 +0200

    Update passivbot.py
    added check if 'telegram' in config, to avoid crash if key 'telegram' is missing from config

--- passivbot.py
+++ passivbot.py
@@ -1521,7 +1521,7 @@ async def main() -> None:
     print('using config')
     print(json.dumps(config, indent=4))
 
-    if config['telegram']:
+    if 'telegram' in config and config['telegram']:
         await _start_telegram(account=account, bot=bot)
     await start_bot(bot)
 
