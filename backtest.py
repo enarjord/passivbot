d077b6c89be158adc1bcdc4c3c3f672d5d3fc854
Eirik Narjord <eirik.narjord@gmail.com>
2021-07-10 21:09:14 +0200

    adapt to sampled_ticks, work in progress

--- backtest.py
+++ backtest.py
@@ -11,7 +11,7 @@ from downloader import Downloader
 from njit_funcs import njit_backtest, round_
 from plotting import dump_plots
 from procedures import prep_config, make_get_filepath, load_live_config, add_argparse_args
-from pure_funcs import create_xk, denumpyize, ts_to_date, analyze_fills
+from pure_funcs import create_xk, denumpyize, ts_to_date, analyze_fills, spotify_live_config
 
 
 def backtest(config: dict, data: (np.ndarray,), do_print=False) -> (list, bool):
@@ -21,8 +21,7 @@ def backtest(config: dict, data: (np.ndarray,), do_print=False) -> (list, bool):
 
 
 def plot_wrap(config, data):
-    n_days = round_((data[2][-1] - data[2][0]) / (1000 * 60 * 60 * 24), 0.1)
-    print('n_days', round_(n_days, 0.1))
+    print('n_days', round_(config['n_days'], 0.1))
     print('starting_balance', config['starting_balance'])
     print('backtesting...')
     sts = time()
@@ -32,13 +31,13 @@ def plot_wrap(config, data):
         print('no fills')
         return
     fdf, result = analyze_fills(fills, {**config, **{'lowest_eqbal_ratio': info[1], 'closest_bkr': info[2]}},
-                                data[2][0], data[2][-1])
+                                data[0][0], data[-1][-1])
     config['result'] = result
     config['plots_dirpath'] = make_get_filepath(os.path.join(
         config['plots_dirpath'], f"{ts_to_date(time())[:19].replace(':', '')}", '')
     )
     fdf.to_csv(config['plots_dirpath'] + "fills.csv")
-    df = pd.DataFrame({**{'price': data[0], 'buyer_maker': data[1], 'timestamp': data[2]},
+    df = pd.DataFrame({**{'timestamp': data[:, 0], 'qty': data[:, 1], 'price': data[:, 2]},
                        **{}})
     print('dumping plots...')
     dump_plots(config, fdf, df)
@@ -58,6 +57,8 @@ async def main():
     live_config = load_live_config(args.live_config_path)
     live_config['long']['enabled'] = config['do_long']
     live_config['shrt']['enabled'] = config['do_shrt']
+    if config['market_type'] == 'spot':
+        live_config = spotify_live_config(live_config)
     config.update(live_config)
     print()
     for k in (keys := ['exchange', 'spot', 'symbol', 'starting_balance', 'start_date', 'end_date',
@@ -65,8 +66,8 @@ async def main():
         if k in config:
             print(f"{k: <{max(map(len, keys)) + 2}} {config[k]}")
     print()
-    data = await downloader.get_data()
-    config['n_days'] = round_((data[2][-1] - data[2][0]) / (1000 * 60 * 60 * 24), 0.1)
+    data = await downloader.get_sampled_ticks()
+    config['n_days'] = round_((data[-1][0] - data[0][0]) / (1000 * 60 * 60 * 24), 0.1)
     pprint.pprint(denumpyize(live_config))
     plot_wrap(config, data)
 
