11f7a6da26353a2e0faa7fa5f798f56669d4c901
Patrick Bosch <patrick.bosch@uantwerpen.be>
2021-07-31 23:50:44 +0200

    Add equal and empty method to Order. Add empty_order and copy_order to create an empty Order an copy an Order.

--- definitions/order.py
+++ definitions/order.py
@@ -1,4 +1,4 @@
-from numba import types, njit, typed
+from numba import types, typed, njit
 from numba.experimental import jitclass
 
 TP = 'TAKE_PROFIT'
@@ -31,19 +31,19 @@ BOTH = 'BOTH'
     ('price', types.float64),
     ('stop_price', types.float64),
     ('qty', types.float64),
-    ('type', types.string),
+    ('order_type', types.string),
     ('side', types.string),
     ('timestamp', types.int64),
     ('action', types.string),
     ('position_side', types.string)
 ])
-class Order:
+class Order(object):
     """
     A class representing an order.
     """
 
-    def __init__(self, symbol: str, order_id: int, price: float, stop_price: float, qty: float, type: str, side: str,
-                 timestamp: int, action: str, position_side: str):
+    def __init__(self, symbol: str, order_id: int, price: float, stop_price: float, qty: float, order_type: str,
+                 side: str, timestamp: int, action: str, position_side: str):
         """
         Creates an order.
         :param symbol: The symbol of the order, for example BTCUSDT.
@@ -52,7 +52,7 @@ class Order:
         :param stop_price: The stop price in case a stop loss or take profit order is created.
         Can be set to 0 for normal orders.
         :param qty: The quantity or size of the order.
-        :param type: The type of the order, for example LIMIT.
+        :param order_type: The type of the order, for example LIMIT.
         :param side: The side of the order, meaning BUY or SELL.
         :param timestamp: The timestamp when the order was executed. Can be set to 0 when creating an order.
         :param action: The action of an order, for example FILLED. Can be set to an empty string when creating an order.
@@ -63,7 +63,7 @@ class Order:
         self.price = price
         self.stop_price = stop_price
         self.qty = qty
-        self.type = type.upper()
+        self.order_type = order_type.upper()
         self.side = side.upper()
         self.timestamp = timestamp
         self.action = action.upper()
@@ -75,18 +75,62 @@ class Order:
         :param order: The order to check against.
         :return: If equal or not.
         """
-        if self.symbol == order.symbol and self.order_id == order.order_id and self.price == order.price \
-                and self.stop_price == order.stop_price and self.qty == order.qty and self.type == order.type \
-                and self.side == order.side and self.timestamp == order.timestamp and self.action == order.action \
-                and self.position_side == order.position_side:
-            return True
-        else:
-            return False
+        return self.symbol == order.symbol and self.order_id == order.order_id and self.price == order.price and \
+               self.stop_price == order.stop_price and self.qty == order.qty and \
+               self.order_type == order.order_type and self.side == order.side and \
+               self.timestamp == order.timestamp and self.action == order.action and \
+               self.position_side == order.position_side
+
+    def empty(self) -> bool:
+        """
+        Checks whether an order is empty. Ignores the symbol.
+        :return: Whether the order is empty or not.
+        """
+        return self.order_id == 0 and self.price == 0.0 and self.stop_price == 0.0 and \
+               self.qty == 0.0 and self.order_type == '' and self.side == '' and self.timestamp == 0 and \
+               self.action == '' and self.position_side == ''
+
+    # A copy method in the class makes the class unpickable and empty_order_list will not work anymore.
+    # def copy(self):
+    #     """
+    #     Creates a new Order object with the current values.
+    #     :return: New Order.
+    #     """
+    #     o = Order(self.symbol, self.order_id, self.price, self.stop_price, self.qty, self.order_type, self.side,
+    #               self.timestamp, self.action, self.position_side)
+    #     return o
+
+
+@njit
+def copy_order(order: Order) -> Order:
+    """
+    Creates a new instance of an Order with the same values as the Order that the function was called with.
+    The order can not directly have a copy method because using the type in the method does not allow to create a typed
+    list.
+    :param order: The order to copy.
+    :return: The copied order.
+    """
+    return Order(order.symbol, order.order_id, order.price, order.stop_price, order.qty, order.order_type, order.side,
+                 order.timestamp, order.action, order.position_side)
 
 
 @njit
 def empty_order_list():
+    """
+    Returns an empty Order typed list.
+    :return: Empty Order typed list
+    """
     l = typed.List()
-    l.append(Order('', 0, 0.0, 0.0, 0.0, '', '', 0, '', ''))
+    l.append(empty_order())
     l.clear()
     return l
+
+
+@njit
+def empty_order():
+    """
+    Returns an empty Order with all values set to 0 or empty strings.
+    :return: Empty Order.
+    """
+    o = Order('', 0, 0.0, 0.0, 0.0, '', '', 0, '', '')
+    return o
