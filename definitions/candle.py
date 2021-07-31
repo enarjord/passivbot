79e7a3e37fb6cff56243cb8dc3a5b748bad6dd05
Patrick Bosch <patrick.bosch@uantwerpen.be>
2021-08-01 00:12:20 +0200

    Add equal and empty method to class and add copy function.

--- definitions/candle.py
+++ definitions/candle.py
@@ -32,10 +32,60 @@ class Candle:
         self.close = close
         self.volume = volume
 
+    def equal(self, candle):
+        """
+        Check for equality between two candles.
+        :param candle: The candle to check against.
+        :return: If equal or not.
+        """
+        return self.timestamp == candle.timestamp and self.open == candle.open and self.high == candle.high and \
+               self.low == candle.low and self.close == candle.close and self.volume == candle.volume
+
+    def empty(self):
+        """
+        Checks whether a candle is empty.
+        :return: Whether the candle is empty or not.
+        """
+        return self.timestamp == 0 and self.open == 0.0 and self.high == 0.0 and self.low == 0.0 and \
+               self.close == 0.0 and self.volume == 0.0
+
+    # def copy(self):
+    #     """
+    #     Creates a new Candle object with the current values.
+    #     :return: New Candle.
+    #     """
+    #     return Candle(self.timestamp, self.open, self.high, self.low, self.close, self.volume)
+
+
+@njit
+def copy_candle(candle: Candle) -> Candle:
+    """
+    Creates a new instance of a Candle with the same values as the Candle that the function was called with.
+    The order can not directly have a copy method because using the type in the method does not allow to create a typed
+    list.
+    :param candle: The candle to copy.
+    :return: The copied candle.
+    """
+    return Candle(candle.timestamp, candle.open, candle.high, candle.low, candle.close, candle.volume)
+
 
 @njit
 def empty_candle_list():
+    """
+    Returns an empty Candle typed list.
+    :return: Empty Candle typed list
+    """
     l = typed.List()
     l.append(Candle(0, 0.0, 0.0, 0.0, 0.0, 0.0))
     l.clear()
     return l
+
+
+@njit
+def empty_candle():
+    """
+    Returns an empty Candle with all values set to 0.
+    :return: Empty Candle.
+    """
+    c = Candle(0, 0.0, 0.0, 0.0, 0.0, 0.0)
+    return c
