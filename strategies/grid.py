42c83c3c7061a1e0f57ad6f437cad6d9d011d359
Patrick Bosch <patrick.bosch@uantwerpen.be>
2021-08-01 17:10:51 +0200

    Use position price everywhere. Adjust calculations to work with it.

--- strategies/grid.py
+++ strategies/grid.py
@@ -5,97 +5,102 @@ from numba import types, typeof, njit
 from numba.experimental import jitclass
 
 from definitions.candle import Candle, empty_candle_list, empty_candle
-from definitions.order import Order, empty_order, empty_order_list, TP, SELL, LONG, LIMIT, BUY, FILLED
-from definitions.order_list import OrderList
+from definitions.order import Order, empty_order, empty_order_list, TP, SELL, LONG, LIMIT, BUY, FILLED, MARKET
 from definitions.position import empty_long_position
 from definitions.position_list import PositionList
-from helpers.optimized import round_down
+from helpers.optimized import round_down, calculate_new_position_size_position_price
 from strategies.base_strategy import Strategy, base_strategy_spec
 
 
 @njit
-def get_initial_position(current_price: float, reentry_grid: np.ndarray, wallet_balance: float, wallet_percent: float,
-                         qty_step: float, price_step: float) -> float:
+def get_initial_position(current_price: float, leverage: float, reentry_grid: np.ndarray, wallet_balance: float,
+                         wallet_percent: float, quantity_step: float, price_step: float) -> float:
     """
-    Calculates the initial unleveraged position size. Uses a percentage of the wallet balance as the limit of the
+    Calculates the initial leveraged position size. Uses a percentage of the wallet balance as the limit of the
     cumulative entries.
     :param current_price: The current price to consider.
+    :param leverage: The leverage to use.
     :param reentry_grid: The grid in form of a 2D array.
     :param wallet_balance: The current wallet balance.
     :param wallet_percent: The percentage of the wallet balance to use.
-    :param qty_step: The quantity step of the pair.
+    :param quantity_step: The quantity step of the pair.
     :param price_step: The price step of the pair.
-    :return: Initial unleveraged position size.
+    :return: Initial leveraged position size.
     """
-    available_balance = wallet_balance * wallet_percent
-    price = current_price
-    mult = 1.0
+    available_leveraged_balance = wallet_balance * wallet_percent * leverage
+    position_price = current_price
+    position_size = 1.0
     sums = np.zeros(len(reentry_grid) + 1)
     sums[0] = current_price
     for i in range(len(reentry_grid)):
-        price = round_down(price * (1 - reentry_grid[i][0] / 100), price_step)
-        mult *= reentry_grid[i][1]
-        sums[i + 1] = price * mult
-    initial_size = round_down(available_balance / np.sum(sums), qty_step)
+        old_position_price = position_price
+        old_position_size = position_size
+        position_price = round_down(position_price * (1 - reentry_grid[i][0] / 100), price_step)
+        position_size = round_down(position_size * reentry_grid[i][1], quantity_step)
+        sums[i + 1] = position_price * position_size
+        position_size, position_price = calculate_new_position_size_position_price(old_position_size,
+                                                                                   old_position_price, position_size,
+                                                                                   position_price, quantity_step)
+    initial_size = round_down(available_leveraged_balance / np.sum(sums), quantity_step)
     return initial_size
 
 
 @njit
-def get_dca_grid(current_price: float, position_size: float, position_price: float, leverage: int,
-                 reentry_grid: np.ndarray, wallet_balance: float, wallet_percent: float, qty_step: float,
-                 price_step: float) -> Tuple[np.ndarray, np.ndarray]:
+def get_dca_grid(position_size: float, position_price: float, leverage: float, reentry_grid: np.ndarray,
+                 wallet_balance: float, wallet_percent: float, quantity_step: float, price_step: float) -> Tuple[
+    np.ndarray, np.ndarray]:
     """
-    Calculates the unleveraged reentry grid based on the current position size and the current price.
-    :param current_price: The current price to consider. Either the price where the initial position was entered, or
-    the price where a take profit enter triggered.
+    Calculates the leveraged reentry grid based on the current position size and the current price.
     :param position_size: The size of the current position.
     :param position_price: The price of the current position.
     :param leverage: The leverage of the position.
     :param reentry_grid: The grid in form of a 2D array.
     :param wallet_balance: The current wallet balance.
     :param wallet_percent: The percentage of the wallet balance to use.
-    :param qty_step: The quantity step of the pair.
+    :param quantity_step: The quantity step of the pair.
     :param price_step: The price step of the pair.
-    :return: The unleveraged reenry values, including price and size.
+    :return: The leveraged reenry values, including price and size.
     """
-    available_balance = wallet_balance * wallet_percent
+    available_leveraged_balance = wallet_balance * wallet_percent * leverage
     reentry_prices = []
     reentry_sizes = []
-    position_size /= leverage
-    max_pos = position_size * position_price
     for v in reentry_grid:
-        current_price = round_down(current_price * (1 - v[0] / 100), price_step)
-        position_size = round_down(position_size * v[1], qty_step)
-        if max_pos + current_price * position_size > available_balance:
+        old_position_price = position_price
+        old_position_size = position_size
+        position_price = round_down(position_price * (1 - v[0] / 100), price_step)
+        position_size = round_down(position_size * v[1], quantity_step)
+        if old_position_price * old_position_size + position_price * position_size > available_leveraged_balance:
             break
-        reentry_prices.append(current_price)
+        reentry_prices.append(position_price)
         reentry_sizes.append(position_size)
-        max_pos += current_price * position_size
+        position_size, position_price = calculate_new_position_size_position_price(old_position_size,
+                                                                                   old_position_price, position_size,
+                                                                                   position_price, quantity_step)
     reentry_prices = np.array(reentry_prices)
     reentry_sizes = np.array(reentry_sizes)
     return reentry_prices, reentry_sizes
 
 
 @njit
-def get_tp_grid(current_price: float, position_size: float, tp_grid: np.ndarray, qty_step: float, price_step: float) -> \
-        Tuple[np.ndarray, np.ndarray]:
+def get_tp_grid(position_price: float, position_size: float, tp_grid: np.ndarray, quantity_step: float,
+                price_step: float) -> Tuple[np.ndarray, np.ndarray]:
     """
     Calculates the leveraged take profit grid based on the current position size and current price.
-    :param current_price: The current price to consider. Either the price where the initial position was entered, or
+    :param position_price: The current price to consider. Either the price where the initial position was entered, or
     the price where a reentry enter triggered.
     :param position_size: The size of the current position.
     :param tp_grid: The grid in form of a 2D array.
-    :param qty_step: The quantity step of the pair.
+    :param quantity_step: The quantity step of the pair.
     :param price_step: The price step of the pair.
     :return: The leveraged take profit values, including price and size.
     """
     tp_prices = np.zeros(len(tp_grid))
     tp_sizes = np.zeros(len(tp_grid))
     for i in range(len(tp_grid)):
-        tp_prices[i] = round_down(current_price * (1 + tp_grid[i][0] / 100), price_step)
-        tp_sizes[i] = round_down(position_size * tp_grid[i][1], qty_step)
+        tp_prices[i] = round_down(position_price * (1 + tp_grid[i][0] / 100), price_step)
+        tp_sizes[i] = round_down(position_size * tp_grid[i][1], quantity_step)
     if np.sum(tp_sizes) < position_size:
-        tp_sizes[-1] = round_down(tp_sizes[-1] + position_size - np.sum(tp_sizes), qty_step)
+        tp_sizes[-1] = round_down(tp_sizes[-1] + position_size - np.sum(tp_sizes), quantity_step)
     return tp_prices, tp_sizes
 
 
@@ -180,18 +185,18 @@ class Grid(Strategy):
         :return:
         """
         round_down(0.01, 0.001)
-        get_initial_position(0.01, np.array([[0.1, 1.0]]), 1, 0.1, 0.01, 0.01)
-        get_dca_grid(0.01, 0.01, 0.01, 1, np.array([[0.1, 1.0]]), 1, 0.1, 0.01, 0.01)
+        get_initial_position(0.01, 1.0, np.array([[0.1, 1.0]]), 1, 0.1, 0.01, 0.01)
+        get_dca_grid(0.01, 0.01, 1, np.array([[0.1, 1.0]]), 1, 0.1, 0.01, 0.01)
         get_tp_grid(0.01, 0.01, np.array([[0.1, 1.0]]), 0.01, 0.01)
         self.on_update(PositionList(), empty_order())
         price_list = empty_candle_list()
         price_list.append(empty_candle())
-        self.make_decision(self.balance, PositionList(), OrderList(), price_list)
-        self.prepare_tp_orders(0.0, PositionList())
+        self.make_decision(price_list)
+        self.prepare_tp_orders(PositionList())
         p = PositionList()
         p.update_long(empty_long_position())
-        self.prepare_reentry_orders(0.0, p)
-        self.calculate_dca_tp(0.0, p)
+        self.prepare_reentry_orders(p)
+        self.calculate_dca_tp(p)
 
     def make_decision(self, balance: float, position: PositionList, orders: OrderList, prices: List[Candle]) -> Tuple[
         List[Order], List[Order]]:
@@ -205,6 +210,12 @@ class Grid(Strategy):
         """
         add_orders = empty_order_list()
         delete_orders = empty_order_list()
+        if self.position.long.empty() and len(self.open_orders.long) == 0:
+            if len(prices) > 0:
+                size = get_initial_position(prices[-1].close, self.leverage, self.reentry_grid, self.balance,
+                                            self.percent,
+                                            self.quantity_step, self.price_step)
+                add_orders.append(Order(self.symbol, 0, 0.0, 0.0, size, MARKET, BUY, 0, '', LONG))
         return add_orders, delete_orders
 
     def on_order_update(self, last_filled_order: Order) -> Tuple[List[Order], List[Order]]:
@@ -265,7 +276,7 @@ class Grid(Strategy):
         add_orders = empty_order_list()
         delete_orders = empty_order_list()
         if self.position.long.size == 0.0 and position.long.size != 0.0:
-            add_orders.extend(self.calculate_dca_tp(last_filled_order.price, position))
+            add_orders.extend(self.calculate_dca_tp(position))
         elif self.position.long.size != 0.0 and position.long.size == 0.0:
             for order in self.open_orders.long:
                 delete_orders.append(order)
@@ -277,7 +288,7 @@ class Grid(Strategy):
                 for order in self.open_orders.long:
                     if order.type == TP:
                         delete_orders.append(order)
-                add_orders.extend(self.prepare_tp_orders(last_filled_order.price, position))
+                add_orders.extend(self.prepare_tp_orders(position))
             elif last_filled_order.type == TP and last_filled_order.position_side == LONG \
                     and last_filled_order.side == SELL:
                 # TP triggered
@@ -285,57 +296,55 @@ class Grid(Strategy):
                 for order in self.open_orders.long:
                     if order.type == LIMIT:
                         delete_orders.append(order)
-                add_orders.extend(self.prepare_reentry_orders(last_filled_order.price, position))
+                add_orders.extend(self.prepare_reentry_orders(position))
             else:
                 # Position was changed but not by the strategy
                 # Recalculate both
                 for order in self.open_orders.long:
                     delete_orders.append(order)
-                add_orders.extend(self.calculate_dca_tp(last_filled_order.price, position))
+                add_orders.extend(self.calculate_dca_tp(position))
         return add_orders, delete_orders
 
-    def prepare_tp_orders(self, price: float, position: PositionList) -> List[Order]:
+    def prepare_tp_orders(self, position: PositionList) -> List[Order]:
         """
         Prepares the take profit orders. Calculates the grid and converts it to order list.
-        :param price: Price to use for grid calculation.
         :param position: Current position.
         :return: Typed list of orders.
         """
         orders = empty_order_list()
-        tp_prices, tp_sizes = get_tp_grid(price, position.long.size, self.tp_grid, self.qty_step, self.price_step)
+        tp_prices, tp_sizes = get_tp_grid(position.long.price, position.long.size, self.tp_grid, self.qty_step,
+                                          self.price_step)
         for i in range(len(tp_prices)):
             order = Order(position.long.symbol, 0, float(tp_prices[i]), float(tp_prices[i]), float(tp_sizes[i]), TP,
                           SELL, 0, '', LONG)
             orders.append(order)
         return orders
 
-    def prepare_reentry_orders(self, price: float, position: PositionList) -> List[Order]:
+    def prepare_reentry_orders(self, position: PositionList) -> List[Order]:
         """
         Prepares the reentry orders. Calculates the grid and converts it to order list.
-        :param price: Price to use for grid calculation.
         :param position: Current position.
         :return: Typed list of orders.
         """
         orders = empty_order_list()
-        reentry_prices, reentry_sizes = get_dca_grid(price, position.long.size, position.long.price,
-                                                     position.long.leverage, self.reentry_grid, self.balance,
-                                                     self.percent, self.qty_step, self.price_step)
+        reentry_prices, reentry_sizes = get_dca_grid(position.long.size, position.long.price, position.long.leverage,
+                                                     self.reentry_grid, self.balance, self.percent, self.qty_step,
+                                                     self.price_step)
         for i in range(len(reentry_prices)):
             order = Order(position.long.symbol, 0, float(reentry_prices[i]), float(reentry_prices[i]),
-                          float(reentry_sizes[i] * float(position.long.leverage)), LIMIT, BUY, 0, '', LONG)
+                          float(reentry_sizes[i]), LIMIT, BUY, 0, '', LONG)
             orders.append(order)
         return orders
 
-    def calculate_dca_tp(self, price: float, position: PositionList) -> List[Order]:
+    def calculate_dca_tp(self, position: PositionList) -> List[Order]:
         """
         Calculates both grids.
-        :param price: Price to use for grid calculation.
         :param position: Current position.
         :return: Typed list of orders.
         """
         orders = empty_order_list()
-        orders.extend(self.prepare_reentry_orders(price, position))
-        orders.extend(self.prepare_tp_orders(price, position))
+        orders.extend(self.prepare_reentry_orders(position))
+        orders.extend(self.prepare_tp_orders(position))
         return orders
 
 
