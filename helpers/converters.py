18bc0dab559550c79ce815a34f45509f0383f776
Patrick Bosch <patrick.d.bosch@gmail.com>
2021-08-13 02:12:09 +0200

    Add missing parameters to statistic frame. Add conversion functions for candle lists and order lists.

--- helpers/converters.py
+++ helpers/converters.py
@@ -6,6 +6,7 @@ from numba import njit
 
 from definitions.candle import Candle
 from definitions.fill import Fill
+from definitions.order import Order
 from definitions.statistic import Statistic
 from definitions.tick import Tick
 
@@ -35,6 +36,34 @@ def candles_to_array(candles: List[Candle]) -> np.ndarray:
         array[i] = np.asarray([candles[i].timestamp, candles[i].open, candles[i].high, candles[i].low, candles[i].close,
                                candles[i].volume], dtype=np.float64)
     return array
+
+
+def candles_to_frame(candles: List[Candle]) -> pd.DataFrame:
+    """
+    Converts a list of Candles into a pandas dataframe.
+    :param candles: The list of Candles to convert.
+    :return: The converted dataframe.
+    """
+    candle_list = []
+    for candle in candles:
+        candle_list.append({'timestamp', candle.timestamp,
+                            'open', candle.open,
+                            'high', candle.high,
+                            'low', candle.low,
+                            'close', candle.close,
+                            'volume', candle.volume})
+    candle_frame = pd.DataFrame(candle_list)
+    return candle_frame
+
+
+def candle_array_to_frame(candles: np.ndarray) -> pd.DataFrame:
+    """
+    Converts an array of candles into a pandas dataframe.
+    :param candles: The array of candles to convert.
+    :return: The converted dataframe.
+    """
+    candle_frame = pd.DataFrame(candles, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
+    return candle_frame
 
 
 def fills_to_frame(fills: List[Fill]) -> pd.DataFrame:
@@ -77,6 +106,30 @@ def statistics_to_frame(statistics: List[Statistic]) -> pd.DataFrame:
                                'equity': statistic.equity,
                                'profit_and_loss_balance': statistic.profit_and_loss_balance,
                                'profit_and_loss_equity': statistic.profit_and_loss_equity,
-                               'position_balance_ratio': statistic.position_balance_ratio})
+                               'position_balance_ratio': statistic.position_balance_ratio,
+                               'equity_balance_ratio': statistic.equity_balance_ratio,
+                               'bankruptcy_distance': statistic.bankruptcy_distance})
     statistic_frame = pd.DataFrame(statistic_list)
     return statistic_frame
+
+
+def orders_to_frame(orders: List[Order]) -> pd.DataFrame:
+    """
+    Converts a list of Orders to a pandas dataframe.
+    :param orders: The list of Orders to convert.
+    :return: The converted dataframe.
+    """
+    order_list = []
+    for order in orders:
+        order_list.append({'symbol': order.symbol,
+                           'order_id': order.order_id,
+                           'price': order.price,
+                           'stop_price': order.stop_price,
+                           'quantity': order.quantity,
+                           'order_type': order.order_type,
+                           'side': order.side,
+                           'timestamp': order.timestamp,
+                           'action': order.action,
+                           'position_side': order.position_side})
+    order_frame = pd.DataFrame(order_list)
+    return order_frame
