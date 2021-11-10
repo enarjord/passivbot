e227f97ff33de117a974f4840f8e7fcdaa0806de
Pedro Algarvio <pedro@algarvio.me>
2021-11-10 15:31:47 +0000

    Allow tweaking logging from the CLI
    Refs #276

--- src/passivbot/cli.py
+++ src/passivbot/cli.py
@@ -1,6 +1,6 @@
 import argparse
-import logging
 import os
+import pathlib
 
 import passivbot.backtest
 import passivbot.batch_optimize
@@ -8,15 +8,32 @@ import passivbot.bot
 import passivbot.downloader
 import passivbot.multi_symbol_optimize
 import passivbot.optimize
+import passivbot.utils.logs
 from passivbot.version import __version__
-
-logging.getLogger("telegram").setLevel(logging.CRITICAL)
 
 
 def main() -> None:
     parser = argparse.ArgumentParser(prog="passivbot", description="PassivBot Crypto Trading")
     parser.add_argument("--version", action="version", version=f"%(prog)s {__version__}")
     parser.add_argument("--nojit", help="disable numba", action="store_true")
+    cli_logging_params = parser.add_argument_group(
+        title="Logging", description="Runtime logging configuration"
+    )
+    cli_logging_params.add_argument(
+        "--log-level",
+        choices=sorted(passivbot.utils.logs.LOG_LEVELS),
+        default="warning",
+        help="CLI logging level. Default: %(default)s",
+    )
+    cli_logging_params.add_argument(
+        "--log-file", type=pathlib.Path, default=None, help="Path to logs file"
+    )
+    cli_logging_params.add_argument(
+        "--log-file-level",
+        choices=sorted(passivbot.utils.logs.LOG_LEVELS),
+        default="warning",
+        help="Logs file logging level. Default: %(default)s",
+    )
     subparsers = parser.add_subparsers(title="PassivBot commands")
     passivbot.bot.setup_parser(subparsers)
     passivbot.backtest.setup_parser(subparsers)
@@ -27,6 +44,11 @@ def main() -> None:
 
     # Parse the CLI arguments
     args: argparse.Namespace = parser.parse_args()
+
+    # Setup logging
+    passivbot.utils.logs.setup_cli_logging(args.log_level)
+    if args.log_file:
+        passivbot.utils.logs.setup_logfile_logging(args.log_file, log_level=args.log_file_level)
 
     if args.nojit:
         # Disable numba JIT compilation
