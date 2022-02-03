12f8668e804366a5781677cdbfc35de49863fb36
enarjord <eirik.narjord@gmail.com>
2022-02-03 16:47:32 +0100

    refresh backtest plotting for v5.4

--- plotting.py
+++ plotting.py
@@ -30,126 +30,68 @@ def dump_plots(
     table.add_row(["Symbol", result["symbol"] if "symbol" in result else "unknown"])
     table.add_row(["No. days", round_dynamic(result["result"]["n_days"], 6)])
     table.add_row(["Starting balance", round_dynamic(result["result"]["starting_balance"], 6)])
-    table.add_row(
-        [
-            "Price action distance mean long",
-            round_dynamic(result["result"]["pa_distance_mean_long"], 6),
-        ]
-    )
-    table.add_row(
-        [
-            "Price action distance max long",
-            round_dynamic(result["result"]["pa_distance_max_long"], 6),
-        ]
-    )
-    table.add_row(
-        [
-            "Price action distance mean short",
-            round_dynamic(result["result"]["pa_distance_mean_short"], 6),
-        ]
-    )
-    table.add_row(
-        [
-            "Price action distance max short",
-            round_dynamic(result["result"]["pa_distance_max_short"], 6),
-        ]
-    )
-    table.add_row([" ", " "])
 
-    if result["long"]["enabled"]:
-        table.add_row([" ", " "])
-        table.add_row(["Long", result["long"]["enabled"]])
-        adg_per_exp = result["result"]["adg_long"] / result["long"]["wallet_exposure_limit"]
-        table.add_row(["ADG per exposure", f"{round_dynamic(adg_per_exp * 100, 3)}%"])
-        table.add_row(
-            ["DG mean std ratio", f"{round_dynamic(result['result']['adg_DGstd_ratio_long'], 4)}"]
-        )
-        table.add_row(["No. inital entries", result["result"]["n_ientries_long"]])
-        table.add_row(["No. reentries", result["result"]["n_rentries_long"]])
-        table.add_row(["No. normal closes", result["result"]["n_normal_closes_long"]])
-        table.add_row(
-            [
-                "Mean hours stuck",
-                round_dynamic(result["result"]["hrs_stuck_avg_long"], 6),
-            ]
-        )
-        table.add_row(
-            [
-                "Max hours stuck",
-                round_dynamic(result["result"]["hrs_stuck_max_long"], 6),
-            ]
-        )
-        profit_color = Fore.RED if result["result"]["pnl_sum_long"] < 0 else Fore.RESET
-        table.add_row(
-            [
-                "PNL sum",
-                f"{profit_color}{round_dynamic(result['result']['pnl_sum_long'], 4)}{Fore.RESET}",
-            ]
-        )
-        table.add_row(["Loss sum", round_dynamic(result["result"]["loss_sum_long"], 4)])
-        table.add_row(["Fee sum", round_dynamic(result["result"]["fee_sum_long"], 4)])
-        table.add_row(["Biggest pos size", result["result"]["biggest_psize_long"]])
-        table.add_row(
-            [
-                "Biggest pos cost",
-                round_dynamic(result["result"]["biggest_psize_quote_long"], 4),
-            ]
-        )
-        table.add_row(
-            [
-                "Average n fills per day",
-                round_dynamic(result["result"]["avg_fills_per_day_long"], 3),
-            ]
-        )
-        table.add_row(["Volume quote", round_dynamic(result["result"]["volume_quote_long"], 6)])
-
-    if result["short"]["enabled"]:
-        table.add_row([" ", " "])
-        table.add_row(["Short", result["short"]["enabled"]])
-        adg_per_exp = result["result"]["adg_short"] / result["short"]["wallet_exposure_limit"]
-        table.add_row(["ADG per exposure", f"{round_dynamic(adg_per_exp * 100, 3)}%"])
-        table.add_row(
-            ["DG mean std ratio", f"{round_dynamic(result['result']['adg_DGstd_ratio_short'], 4)}"]
-        )
-        table.add_row(["No. inital entries", result["result"]["n_ientries_short"]])
-        table.add_row(["No. inital entries", result["result"]["n_ientries_short"]])
-        table.add_row(["No. reentries", result["result"]["n_rentries_short"]])
-        table.add_row(["No. normal closes", result["result"]["n_normal_closes_short"]])
-        table.add_row(
-            [
-                "Mean hours stuck",
-                round_dynamic(result["result"]["hrs_stuck_avg_short"], 6),
-            ]
-        )
-        table.add_row(
-            [
-                "Max hours stuck",
-                round_dynamic(result["result"]["hrs_stuck_max_short"], 6),
-            ]
-        )
-        profit_color = Fore.RED if result["result"]["pnl_sum_short"] < 0 else Fore.RESET
-        table.add_row(
-            [
-                "PNL sum",
-                f"{profit_color}{round_dynamic(result['result']['pnl_sum_short'], 4)}{Fore.RESET}",
-            ]
-        )
-        table.add_row(["Loss sum", round_dynamic(result["result"]["loss_sum_short"], 4)])
-        table.add_row(["Fee sum", round_dynamic(result["result"]["fee_sum_short"], 4)])
-        table.add_row(["Biggest pos size", result["result"]["biggest_psize_short"]])
-        table.add_row(
-            [
-                "Biggest pos cost",
-                round_dynamic(result["result"]["biggest_psize_quote_short"], 4),
-            ]
-        )
-        table.add_row(
-            [
-                "Average n fills per day",
-                round_dynamic(result["result"]["avg_fills_per_day_short"], 3),
-            ]
-        )
-        table.add_row(["Volume quote", round_dynamic(result["result"]["volume_quote_short"], 6)])
+    for side in ["long", "short"]:
+        if result[side]["enabled"]:
+            table.add_row([" ", " "])
+            table.add_row([side.capitalize(), result[side]["enabled"]])
+            adg_per_exp = result["result"][f"adg_{side}"] / result[side]["wallet_exposure_limit"]
+            table.add_row(["ADG per exposure", f"{round_dynamic(adg_per_exp * 100, 3)}%"])
+            table.add_row(
+                ["DG mean std ratio", f"{round_dynamic(result['result']['adg_DGstd_ratio_long'], 4)}"]
+            )
+            table.add_row(
+                [
+                    f"Price action distance mean {side}",
+                    round_dynamic(result["result"][f"pa_distance_mean_{side}"], 6),
+                ]
+            )
+            table.add_row(
+                [
+                    f"Price action distance max {side}",
+                    round_dynamic(result["result"][f"pa_distance_max_{side}"], 6),
+                ]
+            )
+            table.add_row(["No. inital entries", result["result"][f"n_ientries_{side}"]])
+            table.add_row(["No. reentries", result["result"][f"n_rentries_{side}"]])
+            table.add_row(["No. normal closes", result["result"][f"n_normal_closes_{side}"]])
+            table.add_row(
+                [
+                    "Mean hours stuck",
+                    round_dynamic(result["result"][f"hrs_stuck_avg_{side}"], 6),
+                ]
+            )
+            table.add_row(
+                [
+                    "Max hours stuck",
+                    round_dynamic(result["result"][f"hrs_stuck_max_{side}"], 6),
+                ]
+            )
+            profit_color = Fore.RED if result["result"][f"pnl_sum_{side}"] < 0 else Fore.RESET
+            table.add_row(
+                [
+                    "PNL sum",
+                    f"{profit_color}{round_dynamic(result['result'][f'pnl_sum_{side}'], 4)}{Fore.RESET}",
+                ]
+            )
+            table.add_row(["Loss sum", round_dynamic(result["result"][f"loss_sum_{side}"], 4)])
+            table.add_row(["Fee sum", round_dynamic(result["result"][f"fee_sum_{side}"], 4)])
+            table.add_row(["Biggest pos size", result["result"][f"biggest_psize_{side}"]])
+            table.add_row(
+                [
+                    "Biggest pos cost",
+                    round_dynamic(result["result"][f"biggest_psize_quote_{side}"], 4),
+                ]
+            )
+            table.add_row(
+                [
+                    "Average n fills per day",
+                    round_dynamic(result["result"][f"avg_fills_per_day_{side}"], 3),
+                ]
+            )
+            table.add_row(
+                ["Volume quote", round_dynamic(result["result"][f"volume_quote_{side}"], 6)]
+            )
 
     dump_live_config(result, result["plots_dirpath"] + "live_config.json")
     json.dump(denumpyize(result), open(result["plots_dirpath"] + "result.json", "w"), indent=4)
@@ -170,87 +112,56 @@ def dump_plots(
     sdf.equity_short.plot(title="Balance and equity Long", xlabel="Time", ylabel="Balance")
     plt.savefig(f"{result['plots_dirpath']}balance_and_equity_sampled_short.png")
 
-    # TODO... fix
-    return
-    print("plotting backtest in chunks...")
     n_parts = max(3, int(round_up(result["n_days"] / 14, 1.0)))
-    for z in range(n_parts):
-        start_ = z / n_parts
-        end_ = (z + 1) / n_parts
-        print(f"{z} of {n_parts} {start_ * 100:.2f}% to {end_ * 100:.2f}%")
-        fig = plot_fills(
-            df,
-            fdf.iloc[int(len(fdf) * start_) : int(len(fdf) * end_)],
-            title=f"Fills {z+1} of {n_parts}",
-        )
-        if fig is not None:
-            fig.savefig(f"{result['plots_dirpath']}backtest_{z + 1}of{n_parts}.png")
-        else:
-            print("no fills...")
-    print("plotting backtest whole...")
-    fig = plot_fills(df, fdf, plot_whole_df=True, title="Overview Fills")
-    fig.savefig(f"{result['plots_dirpath']}whole_backtest.png")
-    if result["long"]["enabled"]:
-        print("plotting long fills...")
-        plt.clf()
-        fig = plot_fills(df, fdf, side=1, plot_whole_df=True, title="Overview Long Fills")
-        fig.savefig(f"{result['plots_dirpath']}whole_backtest_long.png")
-        print("plotting long initial entry band")
-        spans = [
-            result["long"]["ema_span_0"] * 60,
-            ((result["long"]["ema_span_0"] * result["long"]["ema_span_1"]) ** 0.5) * 60,
-            result["long"]["ema_span_1"] * 60,
-        ]
-        emas = pd.DataFrame(
-            {str(span): df.price.ewm(span=span, adjust=False).mean() for span in spans}
-        )
-        ema_band_lower = emas.min(axis=1)
-        ema_band_upper = emas.max(axis=1)
-        long_ientry_band = ema_band_lower * (1 - result["long"]["initial_eprice_ema_dist"])
-        plt.clf()
-        df.price.plot(style="y-", title="Long Initial Entry Band")
-        long_ientry_band.plot(style="b-.")
-        plt.savefig(f"{result['plots_dirpath']}initial_entry_band_long.png")
-        if result["long"]["auto_unstuck_wallet_exposure_threshold"] != 0.0:
-            print("plotting long unstucking bands...")
-            unstucking_band_lower = ema_band_lower * (1 - result["long"]["auto_unstuck_ema_dist"])
-            unstucking_band_upper = ema_band_lower * (1 + result["long"]["auto_unstuck_ema_dist"])
-            plt.clf()
-            df.price.plot(style="y-", title="Long Auto Unstucking Bands")
-            unstucking_band_lower.plot(style="b-.")
-            unstucking_band_upper.plot(style="r-.")
-            plt.savefig(f"{result['plots_dirpath']}auto_unstuck_bands_long.png")
-    if result["short"]["enabled"]:
-        print("plotting short fills...")
-        plt.clf()
-        fig = plot_fills(df, fdf, side=-1, plot_whole_df=True, title="Overview Short Fills")
-        fig.savefig(f"{result['plots_dirpath']}whole_backtest_short.png")
-        print("plotting short initial entry band")
-        spans = [
-            result["short"]["ema_span_0"] * 60,
-            ((result["short"]["ema_span_0"] * result["short"]["ema_span_1"]) ** 0.5) * 60,
-            result["short"]["ema_span_1"] * 60,
-        ]
-        emas = pd.DataFrame(
-            {str(span): df.price.ewm(span=span, adjust=False).mean() for span in spans}
-        )
-        ema_band_lower = emas.min(axis=1)
-        ema_band_upper = emas.max(axis=1)
-        short_ientry_band = ema_band_lower * (1 + result["short"]["initial_eprice_ema_dist"])
-        plt.clf()
-        df.price.plot(style="y-", title="Short Initial Entry Band")
-        short_ientry_band.plot(style="r-.")
-        plt.savefig(f"{result['plots_dirpath']}initial_entry_band_short.png")
-        if result["short"]["auto_unstuck_wallet_exposure_threshold"] != 0.0:
-            print("plotting short unstucking bands...")
-            unstucking_band_lower = ema_band_lower * (1 - result["short"]["auto_unstuck_ema_dist"])
-            unstucking_band_upper = ema_band_lower * (1 + result["short"]["auto_unstuck_ema_dist"])
-            plt.clf()
-            df.price.plot(style="y-", title="short Auto Unstucking Bands")
-            unstucking_band_lower.plot(style="b-.")
-            unstucking_band_upper.plot(style="r-.")
-            plt.savefig(f"{result['plots_dirpath']}auto_unstuck_bands_short.png")
+    for side, fdf in [("long", longs), ("short", shorts)]:
+        if result[side]["enabled"]:
+            fig = plot_fills(df, fdf, plot_whole_df=True, title=f"Overview Fills {side.capitalize()}")
+            fig.savefig(f"{result['plots_dirpath']}whole_backtest_{side}.png")
+            for z in range(n_parts):
+                start_ = z / n_parts
+                end_ = (z + 1) / n_parts
+                print(f"{side} {z} of {n_parts} {start_ * 100:.2f}% to {end_ * 100:.2f}%")
+                fig = plot_fills(
+                    df,
+                    fdf.iloc[int(len(fdf) * start_) : int(len(fdf) * end_)],
+                    title=f"Fills {side} {z+1} of {n_parts}",
+                )
+                if fig is not None:
+                    fig.savefig(f"{result['plots_dirpath']}backtest_{side}{z + 1}of{n_parts}.png")
+                else:
+                    print(f"no {side} fills...")
 
+            print(f"plotting {side} initial entry band")
+            spans = [
+                result[side]["ema_span_0"] * 60,
+                ((result[side]["ema_span_0"] * result[side]["ema_span_1"]) ** 0.5) * 60,
+                result[side]["ema_span_1"] * 60,
+            ]
+            emas = pd.DataFrame(
+                {
+                    str(span): df.iloc[::100].price.ewm(span=span / 100, adjust=False).mean()
+                    for span in spans
+                }
+            )
+            ema_band_lower = emas.min(axis=1)
+            ema_band_upper = emas.max(axis=1)
+            if side == "long":
+                ientry_band = ema_band_lower * (1 - result[side]["initial_eprice_ema_dist"])
+            else:
+                ientry_band = ema_band_upper * (1 + result[side]["initial_eprice_ema_dist"])
+            plt.clf()
+            df.price.iloc[::100].plot(style="y-", title=f"{side.capitalize} Initial Entry Band")
+            ientry_band.plot(style=f"{('b' if side == 'long' else 'r')}-.")
+            plt.savefig(f"{result['plots_dirpath']}initial_entry_band_{side}.png")
+            if result[side]["auto_unstuck_wallet_exposure_threshold"] != 0.0:
+                print(f"plotting {side} unstucking bands...")
+                unstucking_band_lower = ema_band_lower * (1 - result[side]["auto_unstuck_ema_dist"])
+                unstucking_band_upper = ema_band_upper * (1 + result[side]["auto_unstuck_ema_dist"])
+                plt.clf()
+                df.price.plot(style="y-", title=f"{side.capitalize()} Auto Unstucking Bands")
+                unstucking_band_lower.plot(style="b-.")
+                unstucking_band_upper.plot(style="r-.")
+                plt.savefig(f"{result['plots_dirpath']}auto_unstuck_bands_{side}.png")
     print("plotting pos sizes...")
     plt.clf()
     longs.psize.plot()
