dfab103319235149f0e4d39db8b75f2d51447ab8
tedyptedto <nomp5923@gmail.com>
2022-02-09 15:30:37 +0100

    update script

--- run_server_live.sh
+++ run_server_live.sh
@@ -4,6 +4,6 @@ for i in "${symbols[@]}"
 do
     :
     echo "Running screen on $i"
-    screen -S "tedy_$i" -dm bash -c "cd /home/tedy/Documents/passivbot5.3/passivbot;python3 passivbot.py bybit_tedy $i  configs/live/auto_unstuck_enabled.example.json"
+    screen -S "tedy_$i" -dm bash -c "cd /home/tedy/Documents/passivbot5.3/passivbot;python3 passivbot.py bybit_tedy $i  configs/live/a_tedy.example.json"
 done
 
