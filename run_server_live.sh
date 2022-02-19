455de9f3ee133614025a58959e5df5ad5db115c3
tedyptedto <nomp5923@gmail.com>
2022-02-19 10:41:07 +0100

    delete old scripts

--- run_server_live.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-symbols=(SXPUSDT CELRUSDT CHRUSDT DOGEUSDT CHZUSDT ONEUSDT 1INCHUSDT ENJUSDT RSRUSDT GRTUSDT VETUSDT )
-for i in "${symbols[@]}"
-do
-    :
-    echo "Running screen on $i"
-    screen -S "tedy_$i" -dm bash -c "cd /home/tedy/Documents/passivbot5.4.2tedy/passivbot;python3 passivbot.py bybit_tedy $i  configs/live/a_tedy.json"
-done
-
--- stop_server_live.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-symbols=(SXPUSDT CELRUSDT CHRUSDT DOGEUSDT CHZUSDT ONEUSDT 1INCHUSDT ENJUSDT RSRUSDT GRTUSDT VETUSDT )
-for i in "${symbols[@]}"
-do
-    :
-    echo "Kill screen for $i"
-    screen -S "tedy_$i" -X quit
-done
-
