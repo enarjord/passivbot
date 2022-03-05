e897d6c356465cd89e8ebb51ce7482198458408b
sarmerer <sarmerer@gmail.com>
2022-03-05 00:44:52 +0000

    added script to create manager job

---
+++ manager/create-job.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+then echo "Please run as root"
+    exit
+fi
+
+
+SERVICES=/etc/systemd/system
+SERVICE_NAME=passivbot-manager.service
+MANAGER_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PASSIVBOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
+
+# delete if arg -d is passed
+if [ "$1" == "-d" ]
+then
+    echo "Deleting job"
+    sudo systemctl disable $SERVICE_NAME
+    sudo rm -f $SERVICES/$SERVICE_NAME
+    exit
+fi
+
+echo "Creating service file..."
+cat <<EOF > $SERVICES/$SERVICE_NAME
+[Unit]
+Description=Start passivbot instances on boot
+After=network.target
+
+[Service]
+Type=simple
+User=root
+ExecStart=su $SUDO_USER -c "python3 $PASSIVBOT_DIR/manager start-all"
+WorkingDirectory=$PASSIVBOT_DIR
+Restart=no
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+echo "Enabling service..."
+systemctl enable passivbot-manager.service
+echo "Done"
+echo
+echo "Run this script with -d to delete the job"
