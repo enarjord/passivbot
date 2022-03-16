f4c363ec471ebd11255ec663227d8991d3b59fe0
tedyptedto <nomp5923@gmail.com>
2022-03-16 22:32:38 +0100

    adding gs parameter

--- run_server_live_bybit_tedy.sh
+++ run_server_live_bybit_tedy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 current_pwd=`pwd`
+gs=" -gs "
+gs=""
+
 symbols=(SXPUSDT CELRUSDT CHRUSDT DOGEUSDT CHZUSDT ONEUSDT 1INCHUSDT ENJUSDT RSRUSDT GRTUSDT VETUSDT )
 for i in "${symbols[@]}"
 do
     :
     echo "Running screen on $i"
-    screen -S "bybit_tedy_$i" -dm bash -c "cd ${current_pwd}/;python3 passivbot.py bybit_tedy $i  configs/live/a_tedy.json"
+    screen -S "bybit_tedy_$i" -dm bash -c "cd ${current_pwd}/;python3 passivbot.py $gs bybit_tedy $i  configs/live/a_tedy.json"
 done
 
--- run_server_live_sawyer.sh
+++ run_server_live_sawyer.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 current_pwd=`pwd`
+gs=" -gs "
+gs=""
 symbols=(BNBUSDT MATICUSDT ALICEUSDT )
+
 for i in "${symbols[@]}"
 do
     :
     echo "Running screen on $i"
-    screen -S "sawyer_$i" -dm bash -c "cd ${current_pwd}/;python3 passivbot.py sawyer $i  configs/live/a_sawyer.json"
+    screen -S "sawyer_$i" -dm bash -c "cd ${current_pwd}/;python3 passivbot.py $gs sawyer $i  configs/live/a_sawyer.json"
 done
 
