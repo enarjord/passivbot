975f03b7b0cee57b3ffe6a3ca275babb043c50ec
tedyptedto <nomp5923@gmail.com>
2022-03-20 16:42:50 +0100

    discord

--- scripts/discord/actions/chart.py
+++ scripts/discord/actions/chart.py
@@ -5,6 +5,8 @@ from datetime import datetime
 import discord
 import re
 from plotly.subplots import make_subplots
+import ta 
+
 
 async def chart(message):
 
@@ -13,7 +15,7 @@ async def chart(message):
 
     args = content.split(' ')
     if len(args) < 2: # nb_day non obligatoire
-         await message.channel.send('Mauvais usage. Exemple : !chart BTCUSDT 1d 30')
+         await message.channel.send('Mauvais usage. Exemple : !chart BTCUSDT 1d 30d')
          await message.channel.send('Présente le graphique du BTCUSDT en ut 1d sur les 30 derniers jours')
 
     print(args)
@@ -24,8 +26,18 @@ async def chart(message):
         ut   = re.sub("[^a-z0-9]", "", args[2].lower())
 
     nb_days   = 0
+    phrase_ago = " day ago UTC"
     if len(args) == 4 :
-        nb_days   = int(args[3].lower())
+        nb_days   = int(args[3].lower()[:-1])
+        if (args[3].lower().endswith('d')):
+            phrase_ago = " day ago UTC"
+        elif (args[3].lower().endswith('h')):
+            phrase_ago = " hours ago UTC"
+        elif (args[3].lower().endswith('m')):
+            phrase_ago = " minutes ago UTC"
+        elif (args[3].lower().endswith('s')):
+            phrase_ago = " secondws ago UTC"
+
     if nb_days == 0:
         nb_days = 60
 
@@ -41,21 +53,33 @@ async def chart(message):
 
     # Getting historical data (candle data or kline)
     # candle = client.get_historical_klines(coin, ut, earliest_timestamp)
-    candle = client.get_historical_klines(coin, ut, str(nb_days) + " day ago UTC")
+    candle = client.get_historical_klines(coin, ut, str(nb_days) + phrase_ago)
     # print(candle[1])
 
     eth_df = pd.DataFrame(candle, columns=['dateTime', 'open', 'high', 'low', 'close', 'volume', 'closeTime', 'quoteAssetVolume', 'numberOfTrades', 'takerBuyBaseVol', 'takerBuyQuoteVol', 'ignore'])
     eth_df.dateTime = pd.to_datetime(eth_df.dateTime, unit='ms')
     eth_df.closeTime = pd.to_datetime(eth_df.closeTime, unit='ms')
     eth_df.volume = pd.to_numeric(eth_df.volume)
+    eth_df.open = pd.to_numeric(eth_df.open)
+    eth_df.high = pd.to_numeric(eth_df.high)
+    eth_df.close = pd.to_numeric(eth_df.close)
+    eth_df.low = pd.to_numeric(eth_df.low)
+    eth_df['RSI'] = ta.momentum.RSIIndicator(close=eth_df['close'],window=14).rsi()
 
     print(eth_df.tail(10))
 
 
     # Create subplots and mention plot grid size
-    fig = make_subplots(rows=2, cols=1, shared_xaxes=True, 
-                vertical_spacing=0.03, subplot_titles=('OHLC', 'Volume'), 
-                row_width=[0.2, 0.7])
+    fig = make_subplots(
+                rows=3
+                , cols=1
+                , shared_xaxes=True
+                , vertical_spacing=0.03
+                , subplot_titles=('OHLC', 'Volume', 'RSI')
+                , start_cell='top-left'
+                # , row_width=[ 0.7, 0.15, 0.15]
+                , row_heights=[ 0.8, 0.1, 0.1]
+                )
 
     # Plot OHLC on 1st row
     fig.add_trace(go.Candlestick(x=eth_df['dateTime'],
@@ -65,6 +89,19 @@ async def chart(message):
                     ), 
                     row=1, col=1
     )
+
+
+    # ajout des fibos
+    min_price = eth_df['low'].min()
+    max_price = eth_df['high'].max()
+
+    the_618 = min_price + ((max_price - min_price) * (1-0.618))
+    the_50 = min_price + ((max_price - min_price) * (1-0.5))
+
+    fig.add_hline(min_price, annotation_text="min", annotation_position="top right", line_color="black")
+    fig.add_hline(max_price, annotation_text="max", annotation_position="top right", line_color="black")
+    fig.add_hline(the_618, annotation_text="61.8", annotation_position="top right", line_color="green")
+    fig.add_hline(the_50, annotation_text="50", annotation_position="top right", line_color="yellow")
 
     # Bar trace for volumes on 2nd row without legend
     fig.add_trace(go.Bar(x=eth_df['dateTime'], y=eth_df['volume'], showlegend=False), row=2, col=1)
@@ -82,14 +119,29 @@ async def chart(message):
     #             secondary_y=True)
 
     # # include a go.Bar trace for volumes
-    # fig.add_trace(go.Bar(x=eth_df['dateTime'], y=eth_df['volume']),
-    #             secondary_y=False)
+    rsi_fig = go.Scatter(x=eth_df['dateTime'], y=eth_df['RSI'],
+                    mode='lines',
+                    name='lines')
 
+    fig.add_trace(rsi_fig,secondary_y=False, row=3, col=1)
+    fig.add_trace(go.Scatter(
+        name = "rsi 30",
+        x = [eth_df['dateTime'].min(), eth_df['dateTime'].max()],
+        y = [70, 70],
+        mode = "lines"), row=3, col=1)
+    fig.add_trace(go.Scatter(
+        name = "rsi 30",
+        x = [eth_df['dateTime'].min(), eth_df['dateTime'].max()],
+        y = [30, 30],
+        mode = "lines"), row=3, col=1)
     # fig.layout.yaxis2.showgrid=False
-
+    fig['layout']['yaxis3'].update(title='', range=[0, 100], autorange=False)
 
     # fig.update(layout_xaxis_rangeslider_visible=False)
-
+    fig.update_layout(
+        margin=dict(l=20, r=20, t=20, b=20),
+        paper_bgcolor="white",
+    )
 
     fig.write_image("tmp/name_of_file.jpeg")
     await message.channel.send('Chart '+ coin + " " + ut, file=discord.File('tmp/name_of_file.jpeg'))
--- scripts/discord/discord_bot.py
+++ scripts/discord/discord_bot.py
@@ -4,8 +4,12 @@ from actions.pumpdump import pumpdump
 from actions.long_short import long_short
 from actions.chart import chart
 import os
+import logging
+import traceback
+
 
 # python3 -m pip install python-binance
+# pip install ta
 # pip install discord
 # pip install plotly
 # pip install kaleido
@@ -20,30 +24,41 @@ class MyClient(discord.Client):
         print('------')
 
     async def on_message(self, message):
-        # we do not want the bot to reply to itself
-        if message.author.id == self.user.id:
-            return
+        try:
+            # we do not want the bot to reply to itself
+            if message.author.id == self.user.id:
+                return
 
-        if message.content.startswith('!hello'):
-            await hello(message)
+            if message.content.startswith('!help'):
+                await message.channel.send('Commandes disponibles \n\
+    !hello => just say hello :)\n\
+    !chart => show a chart\n\
+    !ls => longshort ratio\n\
+        ')
 
-        if message.content.startswith('!chart'):
-            await chart(message)
+            if message.content.startswith('!hello'):
+                await hello(message)
 
-        if message.content.startswith('!ls'):
-            await long_short(message)
+            if message.content.startswith('!chart'):
+                await chart(message)
 
-        if 'dump' in message.content:
-            await pumpdump(message, 'dump')
-        elif 'pump' in message.content:
-            await pumpdump(message, 'pump')
-            
+            if message.content.startswith('!ls'):
+                await long_short(message)
 
-        # Garé pour les infos
-        # await message.channel.send('Hello {0.author.mention}'.format(message))
-        # await message.channel.send('Hello {0.author.mention}'.format(message))
-        # await message.channel.send('Hello {0.author}'.format(message))
+            if 'dump' in message.content:
+                await pumpdump(message, 'dump')
+            elif 'pump' in message.content:
+                await pumpdump(message, 'pump')
+                
 
+            # Garé pour les infos
+            # await message.channel.send('Hello {0.author.mention}'.format(message))
+            # await message.channel.send('Hello {0.author.mention}'.format(message))
+            # await message.channel.send('Hello {0.author}'.format(message))
+        except Exception as e:
+            logging.error(traceback.format_exc())
+            # Logs the error appropriately.
+            await message.channel.send('Mauvaise commande.') 
 
 client = MyClient()
 base_dir = os.path.realpath(os.path.dirname(os.path.abspath(__file__))+'/')+'/'
