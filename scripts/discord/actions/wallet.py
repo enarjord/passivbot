b54749df645defa1a9a66f7338833cb810cf5a2a
tedyptedto <nomp5923@gmail.com>
2022-03-22 20:48:12 +0100

    ajout de fct wallet

---
+++ scripts/discord/actions/wallet.py
@@ -0,0 +1,57 @@
+from email import message
+from pybit import HTTP  # supports inverse perp & futures, usdt perp, spot.
+
+import os
+import hjson
+from tabulate import tabulate
+
+async def wallet(message):
+
+    api_keys_file = "../../api-keys.json"
+    api_keys_user = "bybit_tedy"
+    coin_ballance = "USDT"
+
+    def get_equity_and_pnl(api_keys_file, api_keys_user, coin_ballance):
+
+        keys = ""
+        if os.path.exists(api_keys_file) :
+            keys = hjson.load(open(api_keys_file, encoding="utf-8"))
+        else:
+            return {'error' : 'Problem loading keys'}
+
+        
+        session_auth = HTTP(
+            endpoint="https://api.bybit.com",
+            api_key=keys[api_keys_user]['key'],
+            api_secret=keys[api_keys_user]['secret']
+        )
+
+        result = session_auth.get_wallet_balance(coin=coin_ballance)
+
+        if result['ret_code'] != 0:
+            return {'error' : 'bad ret_code'}
+        if result['ret_msg'] != "OK":
+            return {'error' : 'bad ret_code'}
+
+        return {
+                    'error'  : '',
+                    'equity' : '%.4f'%(result['result'][coin_ballance]['equity']),
+                    'cum_realised_pnl' : '%.4f'%(result['result'][coin_ballance]['cum_realised_pnl'])
+        }
+        
+        
+        
+
+
+    wallet_data = get_equity_and_pnl(api_keys_file, api_keys_user, coin_ballance)
+
+    if wallet_data['error'] == "":
+        colonne = 20
+        message_content = \
+        "Equity".ljust(colonne)                  +   "Total Realized PNL".ljust(colonne) + "\n" + \
+        wallet_data['equity'].ljust(colonne)     +   wallet_data['cum_realised_pnl'].ljust(colonne)
+        message_content = message_content.replace('.', ',')
+
+        await message.channel.send("```"+message_content+"```")
+    else:
+        await message.channel.send("Problem :"+wallet_data['error'])
