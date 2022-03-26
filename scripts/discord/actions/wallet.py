e4e90a58600fce470174959666bfebec81f158f1
tedyptedto <nomp5923@gmail.com>
2022-03-26 16:49:12 +0100

    wallet

--- scripts/discord/actions/wallet.py
+++ scripts/discord/actions/wallet.py
@@ -132,8 +132,7 @@ async def wallet(message):
     if wallet_data['error'] == "":
         colonne = 20
         message_content = \
-        "" + user_name + " (Positions : " + str(round(now_data['total_position'])) \
-                         + "$ | Margin used : " + str(round(now_data['used_margin'])) + "$): \n" + \
+        "" + user_name.upper() + " | Positions : " + str(round(now_data['total_position'])) + "$ | Margin used : " + str(round(now_data['used_margin'])) + "$): \n" + \
         "```" + \
         "Equity".ljust(colonne)                                                                 +   "Tot. Rea. PNL".ljust(colonne) + "\n" + \
         str(now_data['equity']).ljust(colonne).replace('.', ',')                                +   str(now_data['cum_realised_pnl']).ljust(colonne).replace('.', ',') + "\n" + \
