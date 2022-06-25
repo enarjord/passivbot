cf8f89d5515d2720303354b0f9463853c742be51
tedyptedto <nomp5923@gmail.com>
2022-06-25 10:56:53 +0200

    cleaning

--- scripts/full_run.sh
+++ scripts/full_run.sh
@@ -1,13 +1,23 @@
 
-# live_config="../configs/live/a_tedy.json"
+
 live_config="../configs/live/a_tedy.json"
 backtest_config="../configs/backtest/default.hjson"
 nb_best_coin="3"
+
+# live_config="../configs/live/a_sawyer.json"
+# backtest_config="../configs/backtest/default_sawyer.hjson"
+# nb_best_coin=7
+
+# live_config="../configs/live/a_pro.json"
+# backtest_config="../configs/backtest/default_pro.hjson"
+# nb_best_coin=20
+
+
 starting_balance="1000.0"
 total_wallet_exposure="3"
 start_date="2021-01-01"
 end_date="2022-06-23"
-user="bybit_tedy"
+user="bybit_tedy" #bybit_tedy sawyer bybit_pro
 
 echo "-------------------------"
 # Update the Starting balance for backtest
@@ -59,15 +69,15 @@ case "$response" in
 esac
 
 
-#python3 0_coin_grid_validator.py ${live_config} ${backtest_config} -mv24 0 -mt24 0
-#python3 1_backtest_looper.py ${live_config} ${backtest_config} -jf tmp/grid_ok_coins.json -oh
+python3 0_coin_grid_validator.py ${live_config} ${backtest_config} -mv24 0 -mt24 0
+python3 1_backtest_looper.py ${live_config} ${backtest_config} -jf tmp/grid_ok_coins.json -oh
 
 
-# python3 2_backtest_summary.py ${nb_best_coin} ${live_config} ${backtest_config} \
-# -min-days 400 \
-# -min-gain 100 \
-# -max-marketcap-pos 20  
-# #-max-stuck-avg 7
-# # -max-stuck 200
+python3 2_backtest_summary.py ${nb_best_coin} ${live_config} ${backtest_config} \
+-min-days 400 \
+-min-gain 100 \
+-max-marketcap-pos 20  
+#-max-stuck-avg 7
+# -max-stuck 200
 
 python3 3_server_script_generator.py  ${user} ${live_config} -jf tmp/best_coins.json
