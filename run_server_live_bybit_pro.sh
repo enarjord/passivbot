4f560731c263af85cddb58ad3a8e0affd8125d48
tedyptedto <nomp5923@gmail.com>
2022-06-26 15:02:30 +0200

    update

--- run_server_live_bybit_pro.sh
+++ run_server_live_bybit_pro.sh
@@ -2,11 +2,11 @@
 current_pwd=`pwd`
 gs=' -gs '
 gs=''
-symbols=(ALICEUSDT CELRUSDT RUNEUSDT FTMUSDT CTKUSDT SXPUSDT MATICUSDT COTIUSDT ONEUSDT YFIUSDT ENJUSDT EGLDUSDT BANDUSDT VETUSDT GRTUSDT OCEANUSDT CVCUSDT ATOMUSDT CRVUSDT IOSTUSDT )
+symbols=(MATICUSDT DOGEUSDT AVAXUSDT SOLUSDT DOTUSDT UNIUSDT XRPUSDT ADAUSDT TRXUSDT LTCUSDT )
 for i in "${symbols[@]}"
 do
     :
     echo "Running screen on $i"
-    screen -S "bybit_pro_$i" -dm bash -c "cd ${current_pwd}/;python3 passivbot.py $gs bybit_pro $i  configs/live/a_pro.json  -m spot "
+    screen -S "bybit_pro_$i" -dm bash -c "cd ${current_pwd}/;python3 passivbot.py $gs bybit_pro $i  configs/live/a_pro.json "
 done
 
