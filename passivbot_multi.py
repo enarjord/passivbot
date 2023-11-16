e5a60c23b3f169509a654778d4d0a6789acf846e
enarjord <eirik.narjord@gmail.com>
2023-11-15 19:30:20 -0500

    begin work on execute_orders_refresh()

--- passivbot_multi.py
+++ passivbot_multi.py
@@ -28,7 +28,6 @@ class Passivbot:
         self.tickers = {}
         self.emas_long = {}
         self.emas_short = {}
-        self.stop_websocket = False
         self.symbol_ids = {}
         self.min_costs = {}
         self.min_qtys = {}
@@ -36,20 +35,35 @@ class Passivbot:
         self.price_steps = {}
         self.c_mults = {}
         self.debug_event_log = []
+        self.stop_websocket = False
         logging.basicConfig(
             format="%(asctime)s %(levelname)-8s %(message)s",
             level=logging.INFO,
             datefmt="%Y-%m-%dT%H:%M:%S",
         )
 
+    async def execute_orders_refresh(self, symbols: [str] = []):
+        # 1. fetch open orders and position via REST
+        # 2. if stuck, fetch pnls via REST
+        # 3. calc orders_to_create and orders_to_cancel
+        # 4. cancel wrong orders
+        # 5. create missing orders
+
+        # if symbols is empty list: update for all symbols
+        # else: update for given symbols
+        pass
+
     async def handle_order_update(self, upd_list):
         try:
             self.debug_event_log.append(upd_list)
             for upd in upd_list:
                 if upd["symbol"] not in self.symbols:
-                    print('debug unknown symbol', upd["symbol"])
+                    print("debug unknown symbol", upd["symbol"])
                     return
-                if upd["status"] == "canceled":
+                if upd["filled"] > 0.0:
+                    # There was a fill, partial or full. Schedule update of open orders, pnls, position.
+                    pass
+                elif upd["status"] == "canceled":
                     # remove order from open_orders
                     self.open_orders[upd["symbol"]] = [
                         elm for elm in self.open_orders[upd["symbol"]] if elm["id"] != upd["id"]
@@ -79,10 +93,7 @@ class Passivbot:
 
     async def handle_ticker_update(self, upd):
         self.upd_timestamps["tickers"][upd["symbol"]] = utc_ms()  # update timestamp
-        if (
-            upd["bid"] != self.tickers[upd["symbol"]]["bid"]
-            or upd["ask"] != self.tickers[upd["symbol"]]["ask"]
-        ):
+        if upd["bid"] != self.tickers[upd["symbol"]]["bid"] or upd["ask"] != self.tickers[upd["symbol"]]["ask"]:
             self.tickers[upd["symbol"]]["bid"] = upd["bid"]
             self.tickers[upd["symbol"]]["ask"] = upd["ask"]
             # pprint.pprint(upd)
