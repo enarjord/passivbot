9b1f0c044a7995c25f085e861391cdd4a7ab11a6
enarjord <eirik.narjord@gmail.com>
2023-11-27 19:53:18 -0700

    further...

--- exchanges_multi/bybit.py
+++ exchanges_multi/bybit.py
@@ -1,9 +1,11 @@
 from passivbot_multi import Passivbot, logging
+from uuid import uuid4
 import ccxt.pro as ccxt_pro
 import ccxt.async_support as ccxt_async
 import pprint
 import asyncio
 import traceback
+import numpy as np
 from pure_funcs import multi_replace, floatify, ts_to_date_utc, calc_hash, determine_pos_side_ccxt
 from procedures import print_async_exception, utc_ms
 
@@ -23,15 +25,20 @@ class BybitBot(Passivbot):
                 "apiKey": self.user_info["key"],
                 "secret": self.user_info["secret"],
                 "password": self.user_info["passphrase"],
+                "headers": {"referer": self.broker_code} if self.broker_code else {},
             }
         )
+        self.max_n_cancellations_per_batch = 20
+        self.max_n_creations_per_batch = 12
 
     async def init_bot(self):
         # require symbols to be formatted to ccxt standard COIN/USDT:USDT
         self.markets = await self.cca.fetch_markets()
         self.markets_dict = {elm["symbol"]: elm for elm in self.markets}
-        approved_symbols, self.approved_symbols_long, self.approved_symbols_short = [], [], []
-        for symbol_ in sorted(set(self.config["symbols_long"] + self.config["symbols_short"])):
+        approved_symbols, self.approved_symbols_long, self.approved_symbols_short = [], {}, {}
+        for symbol_ in sorted(
+            set(list(self.config["symbols_long"]) + list(self.config["symbols_short"]))
+        ):
             symbol = symbol_
             if not symbol.endswith("/USDT:USDT"):
                 coin_extracted = multi_replace(
@@ -59,10 +66,12 @@ class BybitBot(Passivbot):
                             symbol_
                         ]
                     if symbol_ in self.config["symbols_long"]:
-                        self.approved_symbols_long.append(symbol)
+                        self.approved_symbols_long[symbol] = self.config["symbols_long"][symbol_]
                     if symbol_ in self.config["symbols_short"]:
-                        self.approved_symbols_short.append(symbol)
-        logging.info(f"approved symbols: {approved_symbols}")
+                        self.approved_symbols_short[symbol] = self.config["symbols_short"][symbol_]
+        n_prints = 5
+        for i in range(0, len(approved_symbols), n_prints):
+            logging.info(f"approved_symbols: {approved_symbols[i:i+n_prints]}")
         self.symbols = sorted(set(approved_symbols))
         self.quote = "USDT"
         self.inverse = False
@@ -85,9 +94,10 @@ class BybitBot(Passivbot):
             }
             self.upd_timestamps["open_orders"][symbol] = 0.0
             self.upd_timestamps["tickers"][symbol] = 0.0
+            self.upd_timestamps["positions"][symbol] = 0.0
         await super().init_bot()
 
-    async def start_webstockets(self):
+    async def start_websockets(self):
         await asyncio.gather(
             self.watch_balance(),
             self.watch_orders(),
@@ -111,6 +121,9 @@ class BybitBot(Passivbot):
                 if self.stop_websocket:
                     break
                 res = await self.ccp.watch_orders()
+                for i in range(len(res)):
+                    res[i]["position_side"] = determine_pos_side_ccxt(res[i])
+                    res[i]["qty"] = res[i]["amount"]
                 await self.handle_order_update(res)
             except Exception as e:
                 print(f"exception watch_orders", e)
@@ -124,6 +137,8 @@ class BybitBot(Passivbot):
                 if self.stop_websocket:
                     break
                 res = await self.ccp.watch_tickers(symbols)
+                if res["last"] is None:
+                    res["last"] = np.random.choice([res["bid"], res["ask"]])
                 await self.handle_ticker_update(res)
             except Exception as e:
                 print(f"exception watch_tickers {symbols}", e)
@@ -141,6 +156,7 @@ class BybitBot(Passivbot):
                 next_page_cursor = None
                 for elm in fetched:
                     elm["position_side"] = determine_pos_side_ccxt(elm)
+                    elm["qty"] = elm["amount"]
                     open_orders[elm["id"]] = elm
                     if "nextPageCursor" in elm["info"]:
                         next_page_cursor = elm["info"]["nextPageCursor"]
@@ -248,7 +264,6 @@ class BybitBot(Passivbot):
                     break
                 i += 1
                 logging.debug(f"fetching income for more than a week {ts_to_date_utc(sts)}")
-                print(f"fetching income for more than a week {ts_to_date_utc(sts)}")
             return sorted(
                 {elm["orderId"]: elm for elm in income}.values(), key=lambda x: x["updatedTime"]
             )
@@ -279,9 +294,6 @@ class BybitBot(Passivbot):
             while True:
                 if fetched["result"]["list"] == []:
                     break
-                print(
-                    f"fetching income {ts_to_date_utc(fetched['result']['list'][-1]['updatedTime'])}"
-                )
                 logging.debug(
                     f"fetching income {ts_to_date_utc(fetched['result']['list'][-1]['updatedTime'])}"
                 )
@@ -309,3 +321,88 @@ class BybitBot(Passivbot):
             print_async_exception(fetched)
             traceback.print_exc()
             return []
+
+    async def execute_multiple(self, orders: [dict], type_: str, max_n_executions: int):
+        if not orders:
+            return []
+        executions = []
+        for order in orders[:max_n_executions]:  # sorted by PA dist
+            execution = None
+            try:
+                execution = asyncio.create_task(getattr(self, type_)(order))
+                executions.append((order, execution))
+            except Exception as e:
+                logging.error(f"error executing {type_} {order} {e}")
+                print_async_exception(execution)
+                traceback.print_exc()
+        results = []
+        for execution in executions:
+            result = None
+            try:
+                result = await execution[1]
+                results.append(result)
+            except Exception as e:
+                logging.error(f"error executing {type_} {execution} {e}")
+                print_async_exception(result)
+                traceback.print_exc()
+        return results
+
+    async def execute_cancellation(self, order: dict) -> dict:
+        executed = None
+        try:
+            executed = await self.cca.cancel_order(order["id"], symbol=order["symbol"])
+            return {
+                "symbol": executed["symbol"],
+                "side": order["side"],
+                "id": executed["id"],
+                "position_side": order["position_side"],
+                "qty": order["qty"],
+                "price": order["price"],
+            }
+        except Exception as e:
+            logging.error(f"error cancelling order {order} {e}")
+            print_async_exception(executed)
+            traceback.print_exc()
+            return {}
+
+    async def execute_cancellations(self, orders: [dict]) -> [dict]:
+        if len(orders) > self.max_n_cancellations_per_batch:
+            # prioritize cancelling reduce-only orders
+            try:
+                reduce_only_orders = [x for x in orders if x["reduce_only"]]
+                rest = [x for x in orders if not x["reduce_only"]]
+                orders = (reduce_only_orders + rest)[:max_n_cancellations_per_batch]
+            except Exception as e:
+                logging.error("debug filter cancellations {e}")
+        return await self.execute_multiple(
+            orders, "execute_cancellation", self.max_n_cancellations_per_batch
+        )
+
+    async def execute_order(self, order: dict) -> dict:
+        executed = None
+        try:
+            executed = await self.cca.create_limit_order(
+                symbol=order["symbol"],
+                side=order["side"],
+                amount=abs(order["qty"]),
+                price=order["price"],
+                params={
+                    "positionIdx": 1 if order["position_side"] == "long" else 2,
+                    "timeInForce": "postOnly",
+                    "orderLinkId": order["custom_id"] + str(uuid4()),
+                },
+            )
+            if "symbol" not in executed or executed["symbol"] is None:
+                executed["symbol"] = order["symbol"]
+            for key in ["side", "position_side", "qty", "price"]:
+                if key not in executed or executed[key] is None:
+                    executed[key] = order[key]
+            return executed
+        except Exception as e:
+            logging.error(f"error executing order {order} {e}")
+            print_async_exception(executed)
+            traceback.print_exc()
+            return {}
+
+    async def execute_orders(self, orders: [dict]) -> [dict]:
+        return await self.execute_multiple(orders, "execute_order", self.max_n_creations_per_batch)
