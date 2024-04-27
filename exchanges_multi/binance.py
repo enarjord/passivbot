9cd65053ee2b90d1b2f59b2a43d63391d1a0b37e
Eirik Narjord <eirik.narjord@gmail.com>
2024-04-27 13:59:33 -0600

    remove unused code

--- exchanges_multi/binance.py
+++ exchanges_multi/binance.py
@@ -105,39 +105,6 @@ class BinanceBot(Passivbot):
                 traceback.print_exc()
                 await asyncio.sleep(1)
 
-    async def watch_tickersz(self, symbols=None):
-        print("there")
-        self.tmpt = []
-        symbols = list(self.approved_symbols if symbols is None else symbols)
-        await asyncio.gather(*[self.watch_book_ticker(symbol) for symbol in symbols])
-
-    async def watch_book_ticker(self, symbol: str, params={}):
-        """
-        modified watch_ticker to watch_bookTicker
-        """
-        messageHash = f"{self.cca.market(symbol)['lowercaseId']}@bookTicker"
-        type_ = "future"
-        url = f"{self.ccp.urls['api']['ws'][type_]}/{self.ccp.stream(type_, messageHash)}"
-        requestId = self.ccp.request_id(url)
-        request = {"method": "SUBSCRIBE", "params": [messageHash], "id": requestId}
-        subscribe = {"id": requestId}
-        print(url, messageHash, self.ccp.extend(request, params), messageHash, subscribe)
-        while True:
-            try:
-                res = await self.ccp.watch(
-                    url, messageHash, self.ccp.extend(request, params), messageHash, subscribe
-                )
-                self.tmpt.append(res)
-                print(res)
-                if res["symbol"] in self.symbol_ids_inv:
-                    res["symbol"] = self.symbol_ids_inv[res["symbol"]]
-                if "last" not in res or res["last"] is None:
-                    res["last"] = np.random.choice([res["bid"], res["ask"]])
-                self.handle_ticker_update(res)
-            except Exception as e:
-                print(f"exception watch_book_ticker {symbol}", e)
-                traceback.print_exc()
-
     async def fetch_open_orders(self, symbol: str = None, all=False) -> [dict]:
         fetched = None
         open_orders = {}
