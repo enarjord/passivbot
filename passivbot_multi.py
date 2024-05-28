1f3c7943a95e4ed424b1900a0af633982823356d
upperFellow <altlaife@gmail.com>
2024-05-29 00:17:45 +0100

    Update passivbot_multi.py

--- passivbot_multi.py
+++ passivbot_multi.py
@@ -522,9 +522,9 @@ class Passivbot:
                 eligible_symbols = [symbol for symbol in eligible_symbols if self.is_old_enough(symbol) is not False]
                 print('N eligible symbols after is_old_enough filter', len(eligible_symbols))
 
-                if self.config["filter_by_min_effective_cost"]:
-                    eligible_symbols = [symbol for symbol in eligible_symbols if self.effective_min_cost_is_low_enough("long", symbol)]
-                    print('N eligible symbols after filter_by_min_effective_cost long configs', len(eligible_symbols))
+              #  if self.config["filter_by_min_effective_cost"]:
+               #     eligible_symbols = [symbol for symbol in eligible_symbols if self.effective_min_cost_is_low_enough("long", symbol)]
+                #    print('N eligible symbols after filter_by_min_effective_cost long configs', len(eligible_symbols))
                 
                 if self.config["whitelist_from_config_dir_filter"]: 
                     eligible_symbols = [symbol for symbol in eligible_symbols if symbol in self.configs_loaded["live_configs_dir_exact_match"]]
