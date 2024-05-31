3120940e4f11f787767cc649b9f9ae62dd4d9e10
Eirik Narjord <eirik.narjord@gmail.com>
2024-05-31 12:24:49 -0400

    refactoring

--- passivbot-rust/src/backtest.rs
+++ passivbot-rust/src/backtest.rs
@@ -1,6 +1,5 @@
 use ndarray::s;
-use numpy::PyArray2;
-use numpy::PyArray3;
+use numpy::{PyReadonlyArray2, PyReadonlyArray3, PyUntypedArrayMethods};
 use pyo3::prelude::*;
 use pyo3::types::PyDict;
 
@@ -24,35 +23,30 @@ pub struct BacktestConfig {
     unstuck_threshold: f64,
 }
 
-fn extract_value<'a, T: pyo3::FromPyObject<'a>>(dict: &'a &PyDict, key: &str) -> PyResult<T> {
-    dict.get_item(key).map_err(|err| err)?.map_or_else(
-        || {
-            Err(PyErr::new::<pyo3::exceptions::PyKeyError, _>(format!(
-                "Key '{}' not found",
-                key
-            )))
-        },
-        |value| value.extract(),
-    )
+fn extract_value<'a, T: pyo3::FromPyObject<'a>>(dict: &'a PyDict, key: &str) -> PyResult<T> {
+    let item = dict.get_item(key)?.ok_or_else(|| {
+        PyErr::new::<pyo3::exceptions::PyKeyError, _>(format!("Key '{}' not found", key))
+    })?;
+    item.extract()
 }
 
 impl BacktestConfig {
     fn new(dict: &PyDict) -> PyResult<Self> {
-        let close_drawdown_pct = extract_value(&dict, "close_drawdown_pct")?;
-        let close_threshold_pct = extract_value(&dict, "close_threshold_pct")?;
-        let ema_span_0 = extract_value(&dict, "ema_span_0")?;
-        let ema_span_1 = extract_value(&dict, "ema_span_1")?;
-        let initial_ema_dist = extract_value(&dict, "initial_ema_dist")?;
-        let initial_qty_pct = extract_value(&dict, "initial_qty_pct")?;
-        let n_positions = extract_value(&dict, "n_positions")?;
-        let reentry_ddown_factor = extract_value(&dict, "reentry_ddown_factor")?;
-        let reentry_exposure_weighting = extract_value(&dict, "reentry_exposure_weighting")?;
-        let reentry_spacing_factor = extract_value(&dict, "reentry_spacing_factor")?;
-        let total_wallet_exposure_limit = extract_value(&dict, "total_wallet_exposure_limit")?;
-        let unstuck_close_pct = extract_value(&dict, "unstuck_close_pct")?;
-        let unstuck_ema_dist = extract_value(&dict, "unstuck_ema_dist")?;
-        let unstuck_loss_allowance_pct = extract_value(&dict, "unstuck_loss_allowance_pct")?;
-        let unstuck_threshold = extract_value(&dict, "unstuck_threshold")?;
+        let close_drawdown_pct = extract_value(dict, "close_drawdown_pct")?;
+        let close_threshold_pct = extract_value(dict, "close_threshold_pct")?;
+        let ema_span_0 = extract_value(dict, "ema_span_0")?;
+        let ema_span_1 = extract_value(dict, "ema_span_1")?;
+        let initial_ema_dist = extract_value(dict, "initial_ema_dist")?;
+        let initial_qty_pct = extract_value(dict, "initial_qty_pct")?;
+        let n_positions = extract_value(dict, "n_positions")?;
+        let reentry_ddown_factor = extract_value(dict, "reentry_ddown_factor")?;
+        let reentry_exposure_weighting = extract_value(dict, "reentry_exposure_weighting")?;
+        let reentry_spacing_factor = extract_value(dict, "reentry_spacing_factor")?;
+        let total_wallet_exposure_limit = extract_value(dict, "total_wallet_exposure_limit")?;
+        let unstuck_close_pct = extract_value(dict, "unstuck_close_pct")?;
+        let unstuck_ema_dist = extract_value(dict, "unstuck_ema_dist")?;
+        let unstuck_loss_allowance_pct = extract_value(dict, "unstuck_loss_allowance_pct")?;
+        let unstuck_threshold = extract_value(dict, "unstuck_threshold")?;
 
         Ok(BacktestConfig {
             close_drawdown_pct,
@@ -76,8 +70,8 @@ impl BacktestConfig {
 
 #[pyfunction]
 pub fn run_backtest(
-    hlcs: &PyArray3<f64>,
-    noisiness_indices: &PyArray2<i32>,
+    hlcs: PyReadonlyArray3<f64>,
+    noisiness_indices: PyReadonlyArray2<i32>,
     config: &PyDict,
 ) -> PyResult<()> {
     let shape_hlcs = hlcs.shape();
@@ -94,7 +88,7 @@ pub fn run_backtest(
     if shape_hlcs[0] > 0 && shape_hlcs[1] > 0 && shape_hlcs[2] > 0 {
         println!(
             "First element of hlcs: {:?}",
-            hlcs.readonly().as_array().slice(s![0, 0, 0])
+            hlcs.as_array().slice(s![0, 0, 0])
         );
     } else {
         println!("hlcs array is empty");
@@ -103,7 +97,7 @@ pub fn run_backtest(
     if shape_noisiness_indices[0] > 0 && shape_noisiness_indices[1] > 0 {
         println!(
             "First element of noisiness_indices: {:?}",
-            noisiness_indices.readonly().as_array().slice(s![0, 0])
+            noisiness_indices.as_array().slice(s![0, 0])
         );
     } else {
         println!("noisiness_indices array is empty");
