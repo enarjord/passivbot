9943bcb73bd1ce453b61a8b9579a63910e211829
Eirik Narjord <eirik.narjord@gmail.com>
2024-06-02 11:35:35 -0400

    refactoring

--- passivbot-rust/src/backtest.rs
+++ passivbot-rust/src/backtest.rs
@@ -6,7 +6,7 @@ use pyo3::types::PyDict;
 use std::collections::HashSet;
 
 #[pyclass]
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct LiveConfig {
     close_drawdown_pct: f64,
     close_threshold_pct: f64,
@@ -25,8 +25,30 @@ pub struct LiveConfig {
     unstuck_threshold: f64,
 }
 
+impl LiveConfig {
+    fn from_dict(dict: &PyDict) -> PyResult<Self> {
+        Ok(LiveConfig {
+            close_drawdown_pct: extract_value(dict, "close_drawdown_pct")?,
+            close_threshold_pct: extract_value(dict, "close_threshold_pct")?,
+            ema_span_0: extract_value(dict, "ema_span_0")?,
+            ema_span_1: extract_value(dict, "ema_span_1")?,
+            initial_ema_dist: extract_value(dict, "initial_ema_dist")?,
+            initial_qty_pct: extract_value(dict, "initial_qty_pct")?,
+            n_positions: extract_value(dict, "n_positions")?,
+            reentry_ddown_factor: extract_value(dict, "reentry_ddown_factor")?,
+            reentry_exposure_weighting: extract_value(dict, "reentry_exposure_weighting")?,
+            reentry_spacing_factor: extract_value(dict, "reentry_spacing_factor")?,
+            total_wallet_exposure_limit: extract_value(dict, "total_wallet_exposure_limit")?,
+            unstuck_close_pct: extract_value(dict, "unstuck_close_pct")?,
+            unstuck_ema_dist: extract_value(dict, "unstuck_ema_dist")?,
+            unstuck_loss_allowance_pct: extract_value(dict, "unstuck_loss_allowance_pct")?,
+            unstuck_threshold: extract_value(dict, "unstuck_threshold")?,
+        })
+    }
+}
+
 #[pyclass]
-#[derive(Debug)]
+#[derive(Debug, Default)]
 pub struct BacktestConfig {
     long: LiveConfig,
     short: LiveConfig,
@@ -40,20 +62,28 @@ pub struct BacktestConfig {
     min_qtys: Vec<f64>,
 }
 
+impl BacktestConfig {
+    fn from_dict(dict: &PyDict) -> PyResult<Self> {
+        Ok(BacktestConfig {
+            long: LiveConfig::from_dict(extract_value(dict, "long")?)?,
+            short: LiveConfig::from_dict(extract_value(dict, "short")?)?,
+            starting_balance: extract_value(dict, "starting_balance")?,
+            maker_fee: extract_value(dict, "maker_fee")?,
+            c_mults: extract_value(dict, "c_mults")?,
+            symbols: extract_value(dict, "symbols")?,
+            qty_steps: extract_value(dict, "qty_steps")?,
+            price_steps: extract_value(dict, "price_steps")?,
+            min_costs: extract_value(dict, "min_costs")?,
+            min_qtys: extract_value(dict, "min_qtys")?,
+        })
+    }
+}
+
 #[pyclass]
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Default)]
 pub struct Position {
     size: f64,
     price: f64,
-}
-
-impl Default for Position {
-    fn default() -> Self {
-        Position {
-            size: 0.0,
-            price: 0.0,
-        }
-    }
 }
 
 #[pyclass]
@@ -91,84 +121,17 @@ pub struct Stat {
 }
 
 fn extract_value<'a, T: pyo3::FromPyObject<'a>>(dict: &'a PyDict, key: &str) -> PyResult<T> {
-    let item = dict.get_item(key)?.ok_or_else(|| {
-        PyErr::new::<pyo3::exceptions::PyKeyError, _>(format!("Key '{}' not found", key))
-    })?;
-    item.extract()
-}
-
-impl BacktestConfig {
-    fn new(dict: &PyDict) -> PyResult<Self> {
-        let long_dict: &PyDict = extract_value(dict, "long")?;
-        let short_dict: &PyDict = extract_value(dict, "short")?;
-
-        let long = LiveConfig {
-            close_drawdown_pct: extract_value(long_dict, "close_drawdown_pct")?,
-            close_threshold_pct: extract_value(long_dict, "close_threshold_pct")?,
-            ema_span_0: extract_value(long_dict, "ema_span_0")?,
-            ema_span_1: extract_value(long_dict, "ema_span_1")?,
-            initial_ema_dist: extract_value(long_dict, "initial_ema_dist")?,
-            initial_qty_pct: extract_value(long_dict, "initial_qty_pct")?,
-            n_positions: extract_value(long_dict, "n_positions")?,
-            reentry_ddown_factor: extract_value(long_dict, "reentry_ddown_factor")?,
-            reentry_exposure_weighting: extract_value(long_dict, "reentry_exposure_weighting")?,
-            reentry_spacing_factor: extract_value(long_dict, "reentry_spacing_factor")?,
-            total_wallet_exposure_limit: extract_value(long_dict, "total_wallet_exposure_limit")?,
-            unstuck_close_pct: extract_value(long_dict, "unstuck_close_pct")?,
-            unstuck_ema_dist: extract_value(long_dict, "unstuck_ema_dist")?,
-            unstuck_loss_allowance_pct: extract_value(long_dict, "unstuck_loss_allowance_pct")?,
-            unstuck_threshold: extract_value(long_dict, "unstuck_threshold")?,
-        };
-        let short = LiveConfig {
-            close_drawdown_pct: extract_value(short_dict, "close_drawdown_pct")?,
-            close_threshold_pct: extract_value(short_dict, "close_threshold_pct")?,
-            ema_span_0: extract_value(short_dict, "ema_span_0")?,
-            ema_span_1: extract_value(short_dict, "ema_span_1")?,
-            initial_ema_dist: extract_value(short_dict, "initial_ema_dist")?,
-            initial_qty_pct: extract_value(short_dict, "initial_qty_pct")?,
-            n_positions: extract_value(short_dict, "n_positions")?,
-            reentry_ddown_factor: extract_value(short_dict, "reentry_ddown_factor")?,
-            reentry_exposure_weighting: extract_value(short_dict, "reentry_exposure_weighting")?,
-            reentry_spacing_factor: extract_value(short_dict, "reentry_spacing_factor")?,
-            total_wallet_exposure_limit: extract_value(short_dict, "total_wallet_exposure_limit")?,
-            unstuck_close_pct: extract_value(short_dict, "unstuck_close_pct")?,
-            unstuck_ema_dist: extract_value(short_dict, "unstuck_ema_dist")?,
-            unstuck_loss_allowance_pct: extract_value(short_dict, "unstuck_loss_allowance_pct")?,
-            unstuck_threshold: extract_value(short_dict, "unstuck_threshold")?,
-        };
-        let starting_balance = extract_value(dict, "starting_balance")?;
-        let maker_fee = extract_value(dict, "maker_fee")?;
-        let c_mults = extract_value(dict, "c_mults")?;
-        let symbols = extract_value(dict, "symbols")?;
-        let qty_steps = extract_value(dict, "qty_steps")?;
-        let price_steps = extract_value(dict, "price_steps")?;
-        let min_costs = extract_value(dict, "min_costs")?;
-        let min_qtys = extract_value(dict, "min_qtys")?;
-
-        Ok(BacktestConfig {
-            long,
-            short,
-            starting_balance,
-            maker_fee,
-            c_mults,
-            symbols,
-            qty_steps,
-            price_steps,
-            min_costs,
-            min_qtys,
-        })
-    }
+    dict.get_item(key)
+        .map_err(|_| {
+            PyErr::new::<pyo3::exceptions::PyKeyError, _>(format!("Key '{}' not found", key))
+        })?
+        .ok_or_else(|| PyErr::new::<pyo3::exceptions::PyValueError, _>("Value is None"))
+        .and_then(pyo3::FromPyObject::extract)
 }
 
 fn repeat_elements_to_rows(arr: ArrayView1<f64>, n: usize) -> Array2<f64> {
     let rows = arr.len();
-    let mut result = Array2::<f64>::zeros((rows, n));
-    for i in 0..rows {
-        for j in 0..n {
-            result[(i, j)] = arr[i];
-        }
-    }
-    result
+    Array2::from_shape_fn((rows, n), |(i, _)| arr[i])
 }
 
 fn prepare_emas_forager(
@@ -183,30 +146,27 @@ fn prepare_emas_forager(
     Array1<f64>,
     Array1<f64>,
 ) {
-    let mut spans_long_vec = vec![
-        spans_long[0],
-        spans_long[1],
-        (spans_long[0] * spans_long[1]).sqrt(),
-    ];
-    spans_long_vec.sort_by(|a, b| a.partial_cmp(b).unwrap());
-    let spans_long = Array1::from_vec(spans_long_vec).mapv(|x| if x < 1.0 { 1.0 } else { x });
+    let prepare_spans = |spans: [f64; 2]| -> Array1<f64> {
+        let mut vec = vec![spans[0], spans[1], (spans[0] * spans[1]).sqrt()];
+        vec.sort_by(|a, b| a.partial_cmp(b).unwrap());
+        Array1::from_vec(vec).mapv(|x| if x < 1.0 { 1.0 } else { x })
+    };
 
-    let mut spans_short_vec = vec![
-        spans_short[0],
-        spans_short[1],
-        (spans_short[0] * spans_short[1]).sqrt(),
-    ];
-    spans_short_vec.sort_by(|a, b| a.partial_cmp(b).unwrap());
-    let spans_short = Array1::from_vec(spans_short_vec).mapv(|x| if x < 1.0 { 1.0 } else { x });
+    let spans_long = prepare_spans(spans_long);
+    let spans_short = prepare_spans(spans_short);
 
     let hlcs_first_col2 = hlcs_first.column(2);
     let emas_long = repeat_elements_to_rows(hlcs_first_col2, 3);
     let emas_short = repeat_elements_to_rows(hlcs_first_col2, 3);
 
-    let alphas_long = spans_long.mapv(|x| 2.0 / (x + 1.0));
-    let alphas_inv_long = alphas_long.mapv(|x| 1.0 - x);
-    let alphas_short = spans_short.mapv(|x| 2.0 / (x + 1.0));
-    let alphas_inv_short = alphas_short.mapv(|x| 1.0 - x);
+    let compute_alphas = |spans: &Array1<f64>| {
+        let alphas = spans.mapv(|x| 2.0 / (x + 1.0));
+        let alphas_inv = alphas.mapv(|x| 1.0 - x);
+        (alphas, alphas_inv)
+    };
+
+    let (alphas_long, alphas_inv_long) = compute_alphas(&spans_long);
+    let (alphas_short, alphas_inv_short) = compute_alphas(&spans_short);
 
     (
         emas_long,
@@ -220,14 +180,12 @@ fn prepare_emas_forager(
 
 fn update_emas_inplace(
     previous_emas: &mut Array2<f64>,
-    alphas_long: &[f64],
-    alphas_inv_long: &[f64],
+    alphas: &[f64],
+    alphas_inv: &[f64],
     current_prices: &[f64],
 ) {
     for (mut emas, &price) in previous_emas.outer_iter_mut().zip(current_prices) {
-        for (ema, (&alpha, &alpha_inv)) in emas
-            .iter_mut()
-            .zip(alphas_long.iter().zip(alphas_inv_long.iter()))
+        for (ema, (&alpha, &alpha_inv)) in emas.iter_mut().zip(alphas.iter().zip(alphas_inv.iter()))
         {
             *ema = *ema * alpha_inv + price * alpha;
         }
@@ -241,59 +199,15 @@ pub fn run_backtest(
     noisiness_indices: PyReadonlyArray2<i32>,
     config: &Bound<PyAny>,
 ) -> PyResult<Vec<Py<PyArray2<f64>>>> {
-    // Use `extract` method to convert `&Bound<PyAny>` to `&PyDict`
     let dict: &PyDict = config.extract()?;
-    let config = BacktestConfig::new(dict)?;
+    let config = BacktestConfig::from_dict(dict)?;
 
     let enabled_long = config.long.n_positions > 0;
     let enabled_short = config.short.n_positions > 0;
-    let wallet_exposure_limit_long = if enabled_long {
-        config.long.total_wallet_exposure_limit / config.long.n_positions as f64
-    } else {
-        0.0
-    };
-    let wallet_exposure_limit_short = if enabled_short {
-        config.short.total_wallet_exposure_limit / config.short.n_positions as f64
-    } else {
-        0.0
-    };
 
-    let mut positions_long: Array1<Position> =
-        Array1::from_elem(hlcs.shape()[0], Position::default());
-    let mut positions_short: Array1<Position> =
-        Array1::from_elem(hlcs.shape()[0], Position::default());
-
-    let mut has_pos_long: HashSet<i32> = HashSet::new();
-    let mut has_pos_short: HashSet<i32> = HashSet::new();
-    let mut is_stuck_long: HashSet<i32> = HashSet::new();
-    let mut is_stuck_short: HashSet<i32> = HashSet::new();
-    let mut active_longs: HashSet<i32> = HashSet::new();
-    let mut active_shorts: HashSet<i32> = HashSet::new();
-
-    let mut open_orders_entry_long: Vec<OpenOrder> = Vec::new();
-    let mut open_orders_close_long: Vec<OpenOrder> = Vec::new();
-    let mut open_orders_entry_short: Vec<OpenOrder> = Vec::new();
-    let mut open_orders_close_short: Vec<OpenOrder> = Vec::new();
-
-    let mut fills: Vec<Fill> = Vec::new();
-    let mut stats: Vec<Stat> = Vec::new();
-
-    let mut unstuck_order: (i32, i32, OpenOrder) = (
-        0, // pside: 0 (long) / 1 (short)
-        0, // symbol idx
-        OpenOrder {
-            qty: 0.0,
-            price: 0.0,
-            order_type: String::from(""),
-        },
-    );
-
-    let mut pnl_cumsum_max: f64 = 0.0;
-    let mut pnl_cumsum_running: f64 = 0.0;
-
+    let hlcs_array = hlcs.as_array();
     let spans_long = [config.long.ema_span_0, config.long.ema_span_1];
     let spans_short = [config.short.ema_span_0, config.short.ema_span_1];
-    let hlcs_array = hlcs.as_array();
     let hlcs_first = hlcs_array.slice(s![0, .., ..]).to_owned();
 
     let (
@@ -307,21 +221,20 @@ pub fn run_backtest(
 
     let num_steps = hlcs_array.shape()[0];
     let mut tmp_emas = Array3::zeros((num_steps, emas_long.shape()[0], emas_long.shape()[1]));
+
     for k in 0..num_steps {
         if enabled_long {
+            let current_prices = hlcs_array.slice(s![k, .., 2]).to_owned();
             update_emas_inplace(
                 &mut emas_long,
                 alphas_long.as_slice().unwrap(),
                 alphas_inv_long.as_slice().unwrap(),
-                &hlcs_array
-                    .slice(s![k, .., 2])
-                    .to_owned()
-                    .as_slice()
-                    .unwrap(),
+                current_prices.as_slice().unwrap(),
             );
             tmp_emas.slice_mut(s![k, .., ..]).assign(&emas_long);
         }
     }
+
     let py_emas: Vec<Py<PyArray2<f64>>> = tmp_emas
         .outer_iter()
         .map(|ema| PyArray2::from_owned_array(py, ema.to_owned()).to_owned())
