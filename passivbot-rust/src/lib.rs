df661190148c3d35e7276d4ff0bff321fedab4c1
Eirik Narjord <eirik.narjord@gmail.com>
2024-06-03 09:31:13 -0400

    add functions from grid.rs

--- passivbot-rust/src/lib.rs
+++ passivbot-rust/src/lib.rs
@@ -1,7 +1,9 @@
 mod backtest;
+mod grids;
 mod utils;
 
 use backtest::*;
+use grids::*;
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 use utils::*;
@@ -13,6 +15,9 @@ fn passivbot_rust(m: &Bound<'_, PyModule>) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(round_up, m)?)?;
     m.add_function(wrap_pyfunction!(round_dn, m)?)?;
     m.add_function(wrap_pyfunction!(run_backtest, m)?)?;
+    m.add_function(wrap_pyfunction!(cost_to_qty, m)?)?;
+    m.add_function(wrap_pyfunction!(qty_to_cost, m)?)?;
+    m.add_function(wrap_pyfunction!(calc_min_entry_qty, m)?)?;
 
     Ok(())
 }
