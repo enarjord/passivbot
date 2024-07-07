19b86ded413ef5bff49d244572f215eb14ce0fca
Ngô Xuân Mạnh <manhngodh@gmail.com>
2024-07-07 11:55:19 +0700

    update

--- update_supervisor_configs.sh
+++ update_supervisor_configs.sh
@@ -4,7 +4,7 @@ CONFIG_DIR="/opt/passivbot/configs/live"
 SUPERVISOR_CONFIG_DIR="/etc/supervisor/conf.d"
 
 # Clear existing Supervisor configurations for passivbot
-rm -f ${SUPERVISOR_CONFIG_DIR}/passivbot_*.conf
+sudo rm -f ${SUPERVISOR_CONFIG_DIR}/passivbot_*.conf
 
 # Generate Supervisor configurations for each config file
 for config_file in ${CONFIG_DIR}/*.json; do
@@ -16,14 +16,14 @@ for config_file in ${CONFIG_DIR}/*.json; do
 #!/bin/bash
 source /opt/miniconda/etc/profile.d/conda.sh
 conda activate passivbot-env
-python /opt/passivbot/passivbot.py binance_01 ${symbol} ${config_file}
+python /opt/passivbot/passivbot.py binance_01 ${symbol} ${config_file} --leveraage 20
 EOT
 
     # Ensure the script is executable
     chmod +x ${script_path}
 
     # Create Supervisor configuration file for each bot
-    cat <<EOT > ${SUPERVISOR_CONFIG_DIR}/passivbot_${symbol}.conf
+    sudo bash -c "cat <<EOT > ${SUPERVISOR_CONFIG_DIR}/passivbot_${symbol}.conf
 [program:passivbot_${symbol}]
 command=${script_path}
 directory=/opt/passivbot
@@ -31,7 +31,7 @@ autostart=true
 autorestart=true
 stderr_logfile=/var/log/passivbot_${symbol}.err.log
 stdout_logfile=/var/log/passivbot_${symbol}.out.log
-EOT
+EOT"
 done
 
 # Reload Supervisor to apply the new configurations
