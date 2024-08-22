0145cdfce57cb3bea9197cade3b53041dd3af8c6
Eirik Narjord <eirik.narjord@gmail.com>
2024-08-22 12:23:18 -0400

    TIF is variable

--- src/exchanges_forager/bitget.py
+++ src/exchanges_forager/bitget.py
@@ -344,7 +344,9 @@ class BitgetBot(Passivbot):
                     "price": str(order["price"]),
                     "side": self.order_side_map[order["side"]][order["position_side"]],
                     "orderType": "limit",
-                    "timeInForceValue": "post_only",
+                    "timeInForceValue": (
+                        "post_only" if self.config["live"]["time_in_force"] == "post_only" else "gtc"
+                    ),
                 }
             )
             if "msg" in executed and executed["msg"] == "success":
