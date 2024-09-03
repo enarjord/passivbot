e328167f492251689beb9b216d2d053f8ecdb6c5
Eirik Narjord <eirik.narjord@gmail.com>
2024-09-03 13:12:40 -0400

    small print changes

--- src/passivbot_forager.py
+++ src/passivbot_forager.py
@@ -307,7 +307,7 @@ class Passivbot:
 
     async def wait_for_ohlcvs_1m_to_update(self):
         await asyncio.sleep(1.0)
-        prev_print_ts = 0
+        prev_print_ts = utc_ms() - 5000.0
         while self.n_symbols_missing_ohlcvs_1m > self.max_n_concurrent_ohlcvs_1m_updates - 1:
             if utc_ms() - prev_print_ts > 1000 * 10:
                 logging.info(
@@ -858,7 +858,7 @@ class Passivbot:
             if not order or "id" not in order:
                 return False
             if "symbol" not in order or order["symbol"] is None:
-                logging.info(f"{order}")
+                logging.info(f"symbol not in order: {order}")
                 return False
             if order["symbol"] not in self.open_orders:
                 self.open_orders[order["symbol"]] = []
