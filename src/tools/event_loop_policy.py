5f72aaef7a9d9dc9d675f6662cccb7e6f639998a
Ozymo Alt <nvhai.contact@gmail.com>
2025-04-12 10:13:57 +0700

    Create event_loop_policy.py

---
+++ src/tools/event_loop_policy.py
@@ -0,0 +1,6 @@
+import sys
+import asyncio
+
+def set_windows_event_loop_policy():
+    if sys.platform.startswith("win"):
+        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
