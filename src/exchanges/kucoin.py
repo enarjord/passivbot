dc45b9cb40ea488c1b82e9b1c105e26e5d950481
Eirik Narjord <eirik.narjord@gmail.com>
2025-08-21 09:48:00 -0400

    centralize execute_cancellation

--- src/exchanges/kucoin.py
+++ src/exchanges/kucoin.py
@@ -452,24 +452,6 @@ class KucoinBot(Passivbot):
         except:
             return False
 
-    async def execute_cancellation(self, order: dict) -> dict:
-        executed = None
-        try:
-            executed = await self.cca.cancel_order(order["id"], symbol=order["symbol"])
-            return executed
-        except Exception as e:
-            logging.error(f"error cancelling order {order} {e}")
-            print_async_exception(executed)
-            traceback.print_exc()
-            return {}
-
-    async def execute_cancellations(self, orders: [dict]) -> [dict]:
-        if len(orders) == 0:
-            return []
-        if len(orders) == 1:
-            return [await self.execute_cancellation(orders[0])]
-        return await self.execute_multiple(orders, "execute_cancellation")
-
     async def determine_utc_offset(self, verbose=True):
         # returns millis to add to utc to get exchange timestamp
         # call some endpoint which includes timestamp for exchange's server
