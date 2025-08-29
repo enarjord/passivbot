181cfe8c9f23bdb15678ac1254ae857a51423acf
Eirik Narjord <eirik.narjord@gmail.com>
2025-08-29 10:12:40 -0400

    black formatting

--- tests/test_config_utils_parse_overrides.py
+++ tests/test_config_utils_parse_overrides.py
@@ -12,9 +12,7 @@ def _base_config_with_override(coin_key, override_value):
 
 
 def test_parse_overrides_keeps_allowed_mods(monkeypatch):
-    cfg = _base_config_with_override(
-        "btcusdt", {"bot": {"long": {"entry_grid_spacing_pct": 0.03}}}
-    )
+    cfg = _base_config_with_override("btcusdt", {"bot": {"long": {"entry_grid_spacing_pct": 0.03}}})
 
     # Make symbol_to_coin identity to avoid renaming
     monkeypatch.setattr(config_utils, "symbol_to_coin", lambda x: x)
@@ -28,9 +26,7 @@ def test_parse_overrides_keeps_allowed_mods(monkeypatch):
 
 
 def test_parse_overrides_renames_coins(monkeypatch):
-    cfg = _base_config_with_override(
-        "btcusdt", {"bot": {"long": {"entry_grid_spacing_pct": 0.05}}}
-    )
+    cfg = _base_config_with_override("btcusdt", {"bot": {"long": {"entry_grid_spacing_pct": 0.05}}})
 
     # symbol_to_coin will canonicalize the coin name
     monkeypatch.setattr(config_utils, "symbol_to_coin", lambda x: "BTC/USDT")
