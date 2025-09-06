450eb78522d506d44e481d46a273e8d942cf42d4
Eirik Narjord <eirik.narjord@gmail.com>
2025-09-06 19:03:12 -0400

    .

--- src/ohlcvs_1m_manager.py
+++ src/ohlcvs_1m_manager.py
@@ -2,8 +2,6 @@ import logging
 import traceback
 
 
-
-
 logging.basicConfig(
     format="%(asctime)s %(levelname)-8s %(message)s",
     level=logging.INFO,
@@ -12,19 +10,21 @@ logging.basicConfig(
 
 ONE_MIN_MS = 60_000
 
+
 def get_function_name():
     return inspect.currentframe().f_back.f_code.co_name
 
+
 class OHLCVManager:
 
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	def create_lock_file(self, filepath):
-	    try:
-	        open(f"{filepath}.lock", "w").close()
-	        return True
-	    except Exception as e:
-	        logging.error(f"error with {get_function_name()} {e}")
-	        traceback.print_exc()
-	        return False
+    def create_lock_file(self, filepath):
+        try:
+            open(f"{filepath}.lock", "w").close()
+            return True
+        except Exception as e:
+            logging.error(f"error with {get_function_name()} {e}")
+            traceback.print_exc()
+            return False
